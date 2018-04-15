4f78ad1de7b789a9336ce1e99136399bdf80b31c
Mauri Favaron <mafavaron@mac.com>
2018-04-15 08:11:20 +0200

    Create pbl_wind.f90
    Initial commit.

---
+++ core/pbl_wind.f90
@@ -0,0 +1,223 @@
+! pbl_wind - Fortran module, containing routines related to wind
+!            in the Planetary Boundary Layer.
+!
+! This module is part of the pbl_met library.
+!
+! Copyright 2018 by Servizi Territorio srl
+!
+! This is open-source code, covered by the lGPL 3.0 license.
+!
+module pbl_wind
+
+	use pbl_base
+	
+	implicit none
+	
+	private
+	
+	! Public interface
+	! 0. Useful constants and symbols
+	public	:: WCONV_SAME
+	public	:: WCONV_PROVENANCE_TO_FLOW
+	public	:: WCONV_FLOW_TO_PROVENANCE
+	! 1. Conventions
+	public	:: polarToCartesian2
+	public	:: polarToCartesian3
+	public	:: cartesianToPolar2
+	public	:: cartesianToPolar3
+	
+	! Public constants
+	integer, parameter	:: WCONV_SAME               = 0
+	integer, parameter	:: WCONV_PROVENANCE_TO_FLOW = 1
+	integer, parameter	:: WCONV_FLOW_TO_PROVENANCE = 2
+	
+	! Internal constants
+	real, parameter		:: Pi = 3.1415927
+	real, parameter		:: ToRad = Pi/180.
+	real, parameter		:: ToDeg = 180./Pi
+	
+	! Polymorphic interfaces
+	
+contains
+
+	function polarToCartesian2(polar, interpretation) result(cartesian)
+	
+		! Routine arguments
+		real, dimension(2), intent(in)	:: polar				! Wind in polar form (vel=polar(1), dir=polar(2))
+		real, dimension(2)				:: cartesian			! Wind in cartesian form (u=cartesian(1), v=cartesian(2))
+		integer, intent(in), optional	:: interpretation
+		
+		! Locals
+		! --none--
+		
+		! Convert, taking the desired convention into account
+		if(polar(1) > 1.e-6) then
+			if(present(interpretation)) then
+				if(interpretation == WCONV_SAME) then
+					! Same interpretation for input and output: no sign change
+					call uvWind(polar(1), polar(2), cartesian(1), cartesian(2))
+				elseif(interpretation == WCONV_PROVENANCE_TO_FLOW .or. interpretation == WCONV_FLOW_TO_PROVENANCE) then
+					! Different interpretation for input and output: sign change
+					call uvWind(polar(1), polar(2) - 180., cartesian(1), cartesian(2))
+				else
+					! Wrong convention
+					cartesian = [NaN, NaN]
+				end if
+			else
+				! Same interpretation for input and output: no sign change
+				call uvWind(polar(1), polar(2), cartesian(1), cartesian(2))
+			end if
+		else
+			cartesian = [NaN, NaN]
+		end if
+		
+	end function polarToCartesian2
+	
+
+	function polarToCartesian3(polar, interpretation) result(cartesian)
+	
+		! Routine arguments
+		real, dimension(3), intent(in)	:: polar				! Wind in polar form (vel=polar(1), dir=polar(2))
+		real, dimension(3)				:: cartesian			! Wind in cartesian form (u=cartesian(1), v=cartesian(2))
+		integer, intent(in), optional	:: interpretation
+		
+		! Locals
+		! --none--
+		
+		! Convert, taking the desired convention into account
+		if(polar(1) > 1.e-6) then
+			if(present(interpretation)) then
+				if(interpretation == WCONV_SAME) then
+					! Same interpretation for input and output: no sign change
+					call uvWind(polar(1), polar(2), cartesian(1), cartesian(2))
+				elseif(interpretation == WCONV_PROVENANCE_TO_FLOW .or. interpretation == WCONV_FLOW_TO_PROVENANCE) then
+					! Different interpretation for input and output: sign change
+					call uvWind(polar(1), polar(2) - 180., cartesian(1), cartesian(2))
+				else
+					! Wrong convention
+					cartesian = [NaN, NaN, polar(3)]
+				end if
+			else
+				! Same interpretation for input and output: no sign change
+				call uvWind(polar(1), polar(2), cartesian(1), cartesian(2))
+			end if
+		else
+			cartesian = [NaN, NaN, polar(3)]
+		end if
+		
+	end function polarToCartesian3
+	
+
+	function cartesianToPolar2(cartesian, interpretation) result(polar)
+	
+		! Routine arguments
+		real, dimension(2), intent(in)	:: cartesian			! Wind in cartesian form (u=cartesian(1), v=cartesian(2), w=cartesian(3))
+		real, dimension(2)				:: polar				! Wind in polar form (vel=polar(1), dir=polar(2), w=polar(3))
+		integer, intent(in), optional	:: interpretation
+		
+		! Locals
+		! --none--
+		
+		! Convert, taking the desired convention into account
+		if(dot_product(cartesian, cartesian) > 1.e-6) then
+			if(present(interpretation)) then
+				if(interpretation == WCONV_SAME) then
+					! Same interpretation for input and output: no sign change
+					call veldirWind(cartesian(1), cartesian(2), polar(1), polar(2))
+				elseif(interpretation == WCONV_PROVENANCE_TO_FLOW .or. interpretation == WCONV_FLOW_TO_PROVENANCE) then
+					! Different interpretation for input and output: sign change
+					call veldirWind(-cartesian(1), -cartesian(2), polar(1), polar(2))
+				else
+					! Wrong convention
+					polar = [NaN, NaN]
+				end if
+			else
+				! Same interpretation for input and output: no sign change
+				call veldirWind(cartesian(1), cartesian(2), polar(1), polar(2))
+			end if
+		else
+			polar = [NaN, NaN]
+		end if
+		
+	end function cartesianToPolar2
+	
+	
+	function cartesianToPolar3(cartesian, interpretation) result(polar)
+	
+		! Routine arguments
+		real, dimension(3), intent(in)	:: cartesian			! Wind in cartesian form (u=cartesian(1), v=cartesian(2), w=cartesian(3))
+		real, dimension(3)				:: polar				! Wind in polar form (vel=polar(1), dir=polar(2), w=polar(3))
+		integer, intent(in), optional	:: interpretation
+		
+		! Locals
+		! --none--
+		
+		! Convert, taking the desired convention into account
+		if(dot_product(cartesian(1:2), cartesian(1:2)) > 1.e-6) then
+			if(present(interpretation)) then
+				if(interpretation == WCONV_SAME) then
+					! Same interpretation for input and output: no sign change
+					call veldirWind(cartesian(1), cartesian(2), polar(1), polar(2))
+				elseif(interpretation == WCONV_PROVENANCE_TO_FLOW .or. interpretation == WCONV_FLOW_TO_PROVENANCE) then
+					! Different interpretation for input and output: sign change
+					call veldirWind(-cartesian(1), -cartesian(2), polar(1), polar(2))
+				else
+					! Wrong convention
+					polar = [NaN, NaN, cartesian(3)]
+				end if
+			else
+				! Same interpretation for input and output: no sign change
+				call veldirWind(cartesian(1), cartesian(2), polar(1), polar(2))
+			end if
+		else
+			polar = [NaN, NaN, cartesian(3)]
+		end if
+		
+	end function cartesianToPolar3
+	
+	! *********************
+	! * Internal routines *
+	! *********************
+	
+	! Transform horizontal wind components wind from polar to cartesian form
+	! (speed is surely greater than 1.e-6)
+	subroutine uvWind(vel, dir, u, v)
+	
+		! Routine arguments
+		real, intent(in)	:: vel
+		real, intent(in)	:: dir
+		real, intent(out)	:: u
+		real, intent(out)	:: v
+		
+		! Locals
+		! --none--
+		
+		! Perform the transform desired
+		u = vel * sin(dir*ToRad)
+		v = vel * cos(dir*ToRad)
+		
+	end subroutine uvWind
+	
+	
+	! Transform horizontal wind components wind from cartesian to polar form
+	! (speed is surely greater than 1.e-6)
+	subroutine veldirWind(u, v, vel, dir)
+	
+		! Routine arguments
+		real, intent(in)	:: u
+		real, intent(in)	:: v
+		real, intent(out)	:: vel
+		real, intent(out)	:: dir
+		
+		! Locals
+		! --none--
+		
+		! Perform the transform desired
+		vel = sqrt(u**2 + v**2)
+		dir = atan2(u, v)*ToDeg
+		dir = mod(dir, 360.)
+		if(dir < 0.) dir = dir + 360.
+		
+	end subroutine veldirWind
+	
+end module pbl_wind
