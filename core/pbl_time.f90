664b5ff552bfbf2831a54d07c7aec2778cc339d0
Mauri Favaron <mafavaron@mac.com>
2018-04-18 23:32:33 +0200

    Create pbl_time.f90
    Time and astronomy routines, formerly in pal_base module, have been moved here.

---
+++ core/pbl_time.f90
@@ -0,0 +1,479 @@
+! pbl_time - Fortran module, containing computations related to
+!            Planetary Boundary Layer (PBL) quantities, encompassing
+! date and time processing, and some astronomical formulae dealing
+! with sunset/sunrise and apparent solar position.
+!
+! This module is part of the pbl_met library.
+!
+! This is open-source code, covered by the lGPL 3.0 license.
+!
+module pbl_time
+
+	use pbl_base
+
+	implicit none
+	
+	private
+	
+	! Public interface
+	! 1. Date and time management
+	public	:: JulianDay					! Integer-valued Julian day
+	public	:: UnpackDate					! Inverse of integer-valued Julian day
+	public	:: DoW							! Day-of-week
+	public	:: DoY							! Day-of-year, as in old PBL_MET "J_DAY" routine
+	public	:: Leap							! Check a year is leap or not
+	public	:: PackTime						! Date and time to epoch
+	public	:: UnpackTime					! Epoch to date and time
+	! 2. Basic astronomical computations
+	public	:: calcJD						! Fractional Julian day, defined according to NOAA conventions
+	public	:: calcTimeJulianCent			! Fractional Julian century, defined according to NOAA conventions
+	public	:: SinSolarElevation			! Compute the sine of solar elevation angle
+	public	:: SolarDeclination				! Compute the solar declination
+	public	:: SunRiseSunSet				! Old PBL_MET evaluation of sun rise and sun set times (revised)
+	
+contains
+
+	function JulianDay(iYear, iMonth, iDay) result(iJulianDay)
+
+		! Routine arguments
+        integer, intent(in) :: iYear
+        integer, intent(in) :: iMonth
+        integer, intent(in) :: iDay
+        integer             :: iJulianDay
+
+        ! Locals
+        integer     		:: iAuxYear
+        integer     		:: iAuxMonth
+        integer     		:: iCentury
+        integer     		:: iTryJulianDay
+        integer     		:: iNumDays
+        integer, parameter  :: DATE_REFORM_DAY = 588829 ! 15 October 1582, with 31-days months
+        integer, parameter  :: BASE_DAYS       = 1720995
+
+        ! Check year against invalid values. Only positive
+        ! years are supported in this version. Year "0" does
+        ! not exist.
+        if(iYear <= 0) then
+            iJulianDay = -9999
+            return
+        end if
+
+        ! Check month and day to look valid (a rough, non-month-aware
+        ! test is intentionally adopted in sake of simplicity)
+        if((.not.(1<=iMonth .and. iMonth<=12)) .or. (.not.(1<=iDay .and. iDay<=31))) then
+            iJulianDay = -9999
+            return
+        end if
+
+        ! Preliminary estimate the Julian day, based on
+        ! the average duration of year and month in days.
+        if(iMonth > 2) then
+            iAuxYear  = iYear
+            iAuxMonth = iMonth + 1
+        else
+            iAuxYear  = iYear - 1
+            iAuxMonth = iMonth + 13
+        end if
+        iTryJulianDay = floor(YEAR_DURATION * iAuxYear) + &
+                        floor(MONTH_DURATION * iAuxMonth) + &
+                        iDay + BASE_DAYS
+
+        ! Correct estimate if later than the date reform day
+        iNumDays = iDay + 31*iMonth + 372*iYear
+        if(iNumDays >= DATE_REFORM_DAY) then
+            iCentury = 0.01*iAuxYear
+            iJulianDay = iTryJulianDay - iCentury + iCentury/4 + 2
+        else
+            iJulianDay = iTryJulianDay
+        end if
+
+	end function JulianDay
+
+
+    subroutine UnpackDate(iJulianDay, iYear, iMonth, iDay)
+
+        ! Routine arguments
+        integer, intent(in)     :: iJulianDay
+        integer, intent(out)    :: iYear
+        integer, intent(out)    :: iMonth
+        integer, intent(out)    :: iDay
+
+        ! Locals
+        integer :: iDeviation
+        integer :: iPreJulianDay
+        integer :: iPostJulianDay
+        integer :: iYearIndex
+        integer :: iMonthIndex
+        integer :: iDayIndex
+        integer, parameter  :: LIMIT_JULIAN_DAY = 2299161
+        integer, parameter  :: CORRECTION_DAYS  = 1524
+
+        ! Unwind Pope Gregorius' day correction
+        if(iJulianDay >= LIMIT_JULIAN_DAY) then
+            iDeviation = floor(((iJulianDay-1867216)-0.25)/36524.25)
+            iPreJulianDay = iJulianDay + iDeviation - iDeviation/4 + 1
+        else
+            iPreJulianDay = iJulianDay
+        end if
+        iPostJulianDay = iPreJulianDay + CORRECTION_DAYS
+
+        ! Compute time indices
+        iYearIndex  = floor(6680+((iPostJulianDay-2439870)-122.1)/YEAR_DURATION)
+        iDayIndex   = 365*iYearIndex + iYearIndex/4
+        iMonthIndex = floor((iPostJulianDay - iDayIndex)/MONTH_DURATION)
+
+        ! Deduce preliminary date from time indices
+        iDay = iPostJulianDay - floor(MONTH_DURATION*iMonthIndex) - iDayIndex
+        if(iMonthIndex > 13) then
+            iMonth = iMonthIndex - 13
+        else
+            iMonth = iMonthIndex - 1
+        end if
+        iYear = iYearIndex - 4715
+        if(iMonth > 2) iYear = iYear - 1
+
+    end subroutine UnpackDate
+
+
+	! Definition of even-leap year
+	function Leap(ia) result(isLeap)
+	
+		! Routine arguments
+		integer, intent(in)	:: ia
+		logical				:: isLeap
+		
+		! Locals
+		! --none--
+		
+		! Check the year is leap according to the standard definition
+		if(mod(ia,4) /= 0) then
+			! Year, not divisible by 4, is surely even
+			isLeap = .false.
+		else
+			! Year is divisible by 4
+			if(mod(ia,100) == 0) then
+				if(mod(ia,400) == 0) then
+					isLeap = .true.
+				else
+					isLeap = .false.
+				end if
+			else
+				isLeap = .true.
+			end if
+		end if
+		
+	end function Leap
+	
+	
+    function DoW(iJulianDay) result(iDayOfWeek)
+
+        ! Routine arguments
+        integer, intent(in) :: iJulianDay
+        integer         	:: iDayOfWeek
+
+        ! Locals
+        ! -none-
+
+        ! Compute the desired quantity
+        iDayOfWeek = mod(iJulianDay, 7)
+
+    end function DoW
+
+
+	! Day of year
+	function DoY(ia,im,id) result(iDayOfYear)
+
+		! Routine arguments
+		integer, intent(in)	:: ia			! Year (with century)
+		integer, intent(in)	:: im			! Month
+		integer, intent(in)	:: id			! Day
+		integer				:: iDayOfYear	! Day in year
+		
+		! Locals
+		! --none--
+		
+		! Parameters
+		integer, dimension(13,2), parameter	:: ngm = reshape( &
+			[0,31,60,91,121,152,182,213,244,274,305,335,366, &
+			 0,31,59,90,120,151,181,212,243,273,304,334,365], [13,2])
+
+		if(Leap(ia)) then
+			! Leap year
+			iDayOfYear = id+ngm(im,1)
+		else
+			! Even year
+			iDayOfYear = id+ngm(im,2)
+		end if
+
+	end function DoY
+
+
+    subroutine PackTime(iTime, iYear, iMonth, iDay, iInHour, iInMinute, iInSecond)
+
+        ! Routine arguments
+        integer, intent(out)            :: iTime
+        integer, intent(in)             :: iYear
+        integer, intent(in)             :: iMonth
+        integer, intent(in)             :: iDay
+        integer, intent(in), optional   :: iInHour
+        integer, intent(in), optional   :: iInMinute
+        integer, intent(in), optional   :: iInSecond
+
+        ! Locals
+        integer :: iHour
+        integer :: iMinute
+        integer :: iSecond
+        integer :: iJulianDay
+        integer :: iJulianSecond
+
+        ! Check for optional parameters; assign defaults if necessary
+        if(present(iInHour)) then
+            iHour = iInHour
+        else
+            iHour = 0
+        end if
+        if(present(iInMinute)) then
+            iMinute = iInMinute
+        else
+            iMinute = 0
+        end if
+        if(present(iInSecond)) then
+            iSecond = iInSecond
+        else
+            iSecond = 0
+        end if
+        
+        ! Check input parameters for validity
+        if( &
+            iYear   <= 0 .OR. &
+            iMonth  < 1 .OR. iMonth  > 12 .OR. &
+            iDay    < 1 .OR. iDay    > 31 .OR. &
+            iHour   < 0 .OR. iHour   > 23 .OR. &
+            iMinute < 0 .OR. iMinute > 59 .OR. &
+            iSecond < 0 .OR. iSecond > 59 &
+        ) then
+            iTime = -1
+            return
+        end if
+
+        ! Compute based Julian day
+        iJulianDay = JulianDay(iYear, iMonth, iDay) - BASE_DAY
+
+        ! Convert based Julian day to second, and add seconds from time,
+        ! regardless of hour type.
+        iJulianSecond = iJulianDay * 24 * 3600
+        iTime = iJulianSecond + iSecond + 60*(iMinute + 60*iHour)
+
+    end subroutine PackTime
+
+
+    subroutine UnpackTime(iTime, iYear, iMonth, iDay, iHour, iMinute, iSecond)
+
+        ! Routine arguments
+        integer, intent(in)     :: iTime
+        integer, intent(out)    :: iYear
+        integer, intent(out)    :: iMonth
+        integer, intent(out)    :: iDay
+        integer, intent(out)    :: iHour
+        integer, intent(out)    :: iMinute
+        integer, intent(out)    :: iSecond
+
+        ! Locals
+        integer :: iJulianDay
+        integer :: iTimeSeconds
+
+        ! Check parameter
+        if(iTime < 0) then
+            iYear   = 1970
+            iMonth  = 1
+            iDay    = 1
+            iHour   = 0
+            iMinute = 0
+            iSecond = 0
+            return
+        end if
+
+        ! Isolate the date and time parts
+        iJulianDay = iTime/(24*3600) + BASE_DAY
+        iTimeSeconds = mod(iTime, 24*3600)
+
+        ! Process the date part
+        call UnpackDate(iJulianDay, iYear, iMonth, iDay)
+
+        ! Extract time from the time part
+        iSecond = mod(iTimeSeconds,60)
+        iTimeSeconds = iTimeSeconds/60
+        iMinute = mod(iTimeSeconds,60)
+        iHour   = iTimeSeconds/60
+
+    end subroutine UnpackTime
+
+
+	! Fractional Julian day, according to NOAA conventions
+	function calcJD(year, month, day) result(jd)
+
+		! Routine arguments
+		integer, intent(in)	:: year, month, day
+		real				:: jd
+
+		! Locals
+		integer	:: A
+		integer	:: B
+		integer	:: yy
+		integer	:: mm
+
+		! Compute the Julian day corresponding to passed date
+		yy = year
+		mm = month
+		if(mm <= 2) then
+			yy = yy - 1
+			mm = mm + 12
+		end if
+		A = yy/100
+		B = 2 - A + A/4
+		jd = FLOOR(365.25*(yy + 4716)) + FLOOR(30.6001*(mm+1)) + day + B - 1524.5
+
+	end function calcJD
+	
+	
+	! Convert between Julian day and Julian century (unit
+	! of common use in astronomy)
+	function calcTimeJulianCent(jd) result(T)
+
+		! Routine arguments
+		real, intent(in)	:: jd
+		real				:: T
+
+		! Locals
+		! -none-
+
+		! Compute the Julian century
+		T = (jd - 2451545.0)/36525.0
+
+	end function calcTimeJulianCent
+
+
+	function SunRiseSunSet(yy, mo, dy, lat, lon, zone) result(sunRiseSet)
+
+		implicit none
+
+		! Routine arguments
+		integer, intent(in)	:: yy, mo, dy
+		real, intent(in)	:: lat, lon
+		integer, intent(in)	:: zone
+		real, dimension(2)	:: sunRiseSet
+
+		! Locals
+		integer	:: iDayOfYear
+		real	:: solarDeclination
+		real	:: t, b, Sc
+		real	:: centralMeridianLongitude
+		real	:: localLongitude
+		real	:: omegaZeroElev, tZeroElev1, tZeroElev2
+
+		! Parameters
+		real, parameter	:: PI = 3.1415927
+
+		! Compute solar declination
+		iDayOfYear = DoY(yy,mo,dy)
+		solarDeclination = 0.409*SIN(2*PI/365*iDayOfYear - 1.39)
+
+		! Calculate geographical positioning parameters (with a "-" sign for longitudes, according to ASCE conventions)
+		centralMeridianLongitude = -zone*15.0
+		if(centralMeridianLongitude < 0.0) then
+			centralMeridianLongitude = centralMeridianLongitude + 360.0
+		end if
+		localLongitude = -lon
+		if(localLongitude < 0.0) then
+			localLongitude = localLongitude + 360.0
+		end if
+
+		! Calculate seasonal correction for solar time
+		b  = 2.*PI*(iDayOfYear-81)/364.0
+		Sc = 0.1645*SIN(2.0*b) - 0.1255*COS(b) - 0.025*SIN(b)
+
+		! Sunrise and sunset angles
+		omegaZeroElev = ACOS(-TAN(lat*PI/180.0)*TAN(solarDeclination))
+		tZeroElev1 =  omegaZeroElev * 12 / PI + 12.0 - Sc - 0.06667*(centralMeridianLongitude - localLongitude)
+		if(tZeroElev1 < 0.) tZeroElev1 = tZeroElev1 + 12.0
+		tZeroElev2 = -omegaZeroElev * 12 / PI + 12.0 - Sc - 0.06667*(centralMeridianLongitude - localLongitude)
+		if(tZeroElev2 < 0.) tZeroElev2 = tZeroElev2 + 12.0
+		sunRiseSet(1) = MIN(tZeroElev1, tZeroElev2)
+		sunRiseSet(2) = MAX(tZeroElev1, tZeroElev2)
+
+	end function SunRiseSunSet
+
+
+	function SinSolarElevation(yy, mo, dy, hh, mm, ss, lat, lon, zone, averagingPeriod) result(sinBeta)
+
+		implicit none
+
+		! Routine arguments
+		integer, intent(in)	:: yy, mo, dy, hh, mm, ss
+		real, intent(in)	:: lat, lon
+		integer, intent(in)	:: zone
+		integer, intent(in)	:: averagingPeriod
+		real				:: sinBeta
+
+		! Locals
+		integer	:: iDayOfYear
+		real	:: solarDeclination
+		real	:: t, b, Sc
+		real	:: centralMeridianLongitude
+		real	:: localLongitude
+		real	:: omega
+
+		! Parameters
+		real, parameter	:: PI = 3.1415927
+
+		! Compute solar declination
+		iDayOfYear = DoY(yy,mo,dy)
+		solarDeclination = 0.409*SIN(2*PI/365*iDayOfYear - 1.39)
+
+		! Compute current hour at mid of averaging period
+		t = hh + mm/60.0 + ss/3600.0 + 0.5 * averagingPeriod / 3600.0
+
+		! Calculate geographical positioning parameters (with a "-" sign for longitudes, according to ASCE conventions)
+		centralMeridianLongitude = -zone*15.0
+		if(centralMeridianLongitude < 0.0) then
+			centralMeridianLongitude = centralMeridianLongitude + 360.0
+		end if
+		localLongitude = -lon
+		if(localLongitude < 0.0) then
+			localLongitude = localLongitude + 360.0
+		end if
+
+		! Calculate seasonal correction for solar time
+		b  = 2.*PI*(iDayOfYear-81)/364.0
+		Sc = 0.1645*SIN(2.0*b) - 0.1255*COS(b) - 0.025*SIN(b)
+
+		! Solar time angle at midpoint of averaging time
+		omega = (PI/12.0) * ((t + 0.06667*(centralMeridianLongitude - localLongitude) + Sc) - 12.0)
+
+		! Sine of solar elevation angle
+		sinBeta = SIN(lat*PI/180.0)*SIN(solarDeclination) + COS(lat*PI/180.0)*COS(solarDeclination)*COS(omega)
+
+	end function SinSolarElevation
+
+
+	function SolarDeclination(yy, mo, dy) result(sunDecl)
+
+		implicit none
+
+		! Routine arguments
+		integer, intent(in)	:: yy, mo, dy
+		real				:: sunDecl
+
+		! Locals
+		integer	:: iDayOfYear
+
+		! Parameters
+		real, parameter	:: PI = 3.1415927
+
+		! Compute solar declination
+		iDayOfYear = DoY(yy,mo,dy)
+		sunDecl = 0.409*SIN(2.*PI/365.*iDayOfYear - 1.39)
+
+	end function SolarDeclination
+
+end module pbl_time
