735daa39e6fc3c76cdc513a90fe10d0014914a23
Mauri Favaron <mafavaron@mac.com>
2018-04-20 22:39:39 +0200

    Create pal_met.f90
    This module collects all other pal_met modules, providing a unique access point

---
+++ core/pal_met.f90
@@ -0,0 +1,18 @@
+! pbl_met - Fortran module, collecting all other modules and providing
+!           an unique access point to pbl_met library.
+!
+! This module is part of the pbl_met library.
+!
+! This is open-source code, covered by the lGPL 3.0 license.
+!
+module pbl_met
+
+	use pbl_base
+	use pbl_time
+	use pbl_thermo
+	use pbl_evtrn
+	use pbl_wind
+	
+	implicit none
+	
+end module pbl_met
