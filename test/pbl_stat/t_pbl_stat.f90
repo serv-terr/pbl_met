c092b5a542350c931be16d5a86e9c4d6cb8ee9c0
Mauri Favaron <mafavaron@mac.com>
2018-04-22 20:49:27 +0200

    Update t_pbl_stat.f90
    Code restructured for readability; boundary case for unallocated vector dropped from testRangeInvalidate as illegal in Fortran.

--- test/pbl_stat/t_pbl_stat.f90
+++ test/pbl_stat/t_pbl_stat.f90
@@ -11,39 +11,57 @@ program test_pbl_stat
 	implicit none
 	
 	! Locals
-	real, dimension(:), allocatable	:: rvX
-	real, dimension(:), allocatable	:: rvY
+	
+	! Perform actual tests
+	
+	call testRangeInvalidate()
+	
+contains
 
-	! RangeInvalidate
-	! -1- Normal case
-	allocate(rvX(5))
-	rvX = [-70., -40., 0., 60., 180.]
-	call RangeInvalidate(rvX, -40., 60.)
-	print *, "RangeInvalidate - Test 1"
-	print *, "Expected: ", [NaN, -40., 0., 60., NaN]
-	print *, "Found:    ", rvX
-	print *
-	! -1- Normal case
-	rvX = [-70., -40., NaN, 60., 180.]
-	call RangeInvalidate(rvX, -40., 60.)
-	print *, "RangeInvalidate - Test 2"
-	print *, "Expected: ", [NaN, -40., NaN, 60., NaN]
-	print *, "Found:    ", rvX
-	print *
-	! -1- Boundary case: signal is not allocated
-	deallocate(rvX)
-	call RangeInvalidate(rvX, -40., 60.)
-	print *, "RangeInvalidate - Test 3"
-	print *, "Expected: "
-	print *, "Found:    ", rvX
-	print *
-	! -1- Boundary case: zero-length signal
-	deallocate(rvX)
-	allocate(rvX(0))
-	call RangeInvalidate(rvX, -40., 60.)
-	print *, "RangeInvalidate - Test 4"
-	print *, "Expected: "
-	print *, "Found:    ", rvX
-	print *
+	subroutine testRangeInvalidate()
+	
+		! Routine arguments
+		! -none-
+		
+		! Locals
+		real, dimension(:), allocatable	:: rvX
+		
+		! Normal case
+		allocate(rvX(5))
+		rvX = [-70., -40., 0., 60., 180.]
+		call RangeInvalidate(rvX, -40., 60.)
+		print *, "RangeInvalidate - Test 1"
+		print *, "Expected: ", [NaN, -40., 0., 60., NaN]
+		print *, "Found:    ", rvX
+		print *
+		
+		! Normal case
+		rvX = [-70., -40., NaN, 60., 180.]
+		call RangeInvalidate(rvX, -40., 60.)
+		print *, "RangeInvalidate - Test 2"
+		print *, "Expected: ", [NaN, -40., NaN, 60., NaN]
+		print *, "Found:    ", rvX
+		print *
+		
+		! Boundary case: signal is not allocated
+		!     Dropped from test list, as this code block
+		!     is illegal in Fortran.
+		!deallocate(rvX)
+		!call RangeInvalidate(rvX, -40., 60.)
+		!print *, "RangeInvalidate - Test 3"
+		!print *, "Expected: "
+		!print *, "Found:    ", rvX
+		!print *
+		
+		! Boundary case: zero-length signal
+		deallocate(rvX)
+		allocate(rvX(0))
+		call RangeInvalidate(rvX, -40., 60.)
+		print *, "RangeInvalidate - Test 3"
+		print *, "Expected: "
+		print *, "Found:    ", rvX
+		print *
+		
+	end subroutine testRangeInvalidate
 	
 end program test_pbl_stat
