b789effaa7353cbc8cd353fd2d3174ab14d43dc3
Mauri Favaron <mafavaron@mac.com>
2018-04-23 05:29:45 +0200

    Create pbl_simil.f90
    Placeholder added for pal_simil

---
+++ core/pbl_simil.f90
@@ -0,0 +1,20 @@
+! pbl_simil  : Fortran module, providing support to Monin-Obukhov
+!              (surface layer) similarity.
+!
+! This module is part of the pbl_met library.
+!
+! This is open-source code, covered by the lGPL 3.0 license.
+!
+module pbl_simil
+
+    use pbl_base
+    use pbl_wind
+    use pbl_thermo
+
+    implicit none
+    
+    private
+    
+    ! Public interface
+    
+end module pbl_simil
