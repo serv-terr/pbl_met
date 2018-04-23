87d5d1a853d31e24ed38a88275e457c4e28e7ba5
Mauri Favaron <mafavaron@mac.com>
2018-04-23 05:28:46 +0200

    Create pbl_depth.f90
    Placeholder added for pbl_depth module

---
+++ core/pbl_depth.f90
@@ -0,0 +1,21 @@
+! pbl_depth  : Fortran module, providing support to the estimation of
+!              mixing height and other quantities in the full depth of
+!              the Planetary Boundary Layer.
+!
+! This module is part of the pbl_met library.
+!
+! This is open-source code, covered by the lGPL 3.0 license.
+!
+module pbl_depth
+
+    use pbl_base
+    use pbl_wind
+    use pbl_simil
+
+    implicit none
+    
+    private
+    
+    ! Public interface
+    
+end module pbl_depth
