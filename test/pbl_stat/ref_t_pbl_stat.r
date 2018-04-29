34bdef0bbc56a6762a3fe36a9833e6aee172005a
Mauri Favaron <mafavaron@mac.com>
2018-04-29 14:50:25 +0200

    Update ref_t_pbl_stat.r
    Added reference case for partial autocorrelation

--- test/pbl_stat/ref_t_pbl_stat.r
+++ test/pbl_stat/ref_t_pbl_stat.r
@@ -35,3 +35,31 @@ test.acor <- function() {
   return(a)
 }
 
+
+# Normal case, partial autcorrelation, test 1
+test.pacf <- function() {
+  x <- c(1.0, 1.9, 3.1, 3.9, 5.2)
+  d <- acf(x, lag.max=4, type="correlation")
+  print(d$acf[2:5])
+  print(durbin.levinson(d$acf[2:5]))
+  a <- acf(x, lag.max=4, type="partial")
+  return(a)
+}
+
+durbin.levinson <-
+  function(rho)
+  {
+    n = length(rho)
+    pacf = phi = numeric(n)
+    pacf[1] = phi[1] = rho[1]
+    for(k in 2:n) {
+      km1 = k - 1
+      numer = rho[k] - sum(phi[1:km1]*rho[km1:1])
+      denom = 1 - sum(phi[1:km1]*rho[1:km1])
+      phi[k] = numer / denom
+      phi[1:km1] = phi[1:km1] - phi[k] * phi[km1:1]
+      pacf[k] = phi[k]
+    }
+    pacf
+  }
+
