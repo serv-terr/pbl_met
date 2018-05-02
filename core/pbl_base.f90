249654b7d7e71a48819e7b715f5a603eda072232
Mauri Favaron <mafavaron@mac.com>
2018-05-02 04:46:55 +0200

    Update pbl_base.f90
    Support added to double precision non-signalling NaN (NaN_8)

--- core/pbl_base.f90
+++ core/pbl_base.f90
@@ -14,7 +14,8 @@ module pbl_base
 	
 	! Public interface
 	! 0. Useful constants and symbols
-	public	:: NaN							! Non-signalling NaN (generates other NaNs when combined with other values)
+	public	:: NaN							! Non-signalling single precision NaN (generates other NaNs when combined with other values)
+	public	:: NaN_8						! Non-signalling double precision NaN (generates other NaNs when combined with other values)
 	public	:: LAI_GRASS
 	public	:: LAI_ALFALFA
 	public	:: ASCE_STANDARDATMOSPHERE
@@ -31,7 +32,8 @@ module pbl_base
 	public	:: operator(.invalid.)
 	
 	! Constants
-    real, parameter		:: NaN				       = Z'7FC00000'	! Special case of non-signalling NaN
+    real, parameter		:: NaN				       = Z'7FC00000'			! Special case of non-signalling NaN (single precision)
+    real(8), parameter	:: NaN_8			       = Z'7FF8000000000000'	! Special case of non-signalling NaN (double precision)
 	real, parameter		:: YEAR_DURATION	       = 365.25
 	real, parameter		:: MONTH_DURATION	       = 30.6001
 	integer, parameter	:: BASE_DAY			       = 2440588		! 01. 01. 1970
@@ -49,15 +51,18 @@ module pbl_base
 	
 	interface operator(.valid.)
 		module procedure isValid
+		module procedure isValid8
 	end interface operator(.valid.)
 	
 	interface operator(.invalid.)
 		module procedure isInvalid
+		module procedure isInvalid8
 	end interface operator(.invalid.)
 	
 contains
 
 	! Check a value is valid, that is, not a NaN.
+	
 	pure elemental function isValid(value) result(valid)
 	
 		! Routine arguments
@@ -72,8 +77,23 @@ contains
 		
 	end function isValid
 	
+	pure elemental function isValid8(value) result(valid)
+	
+		! Routine arguments
+		real(8), intent(in)	:: value	! Value to check
+		logical				:: valid	! Check result
+		
+		! Locals
+		! -none-
+		
+		! Check validity
+		valid = .not.isnan(value)
+		
+	end function isValid8
+	
 
 	! Check a value is invalid, that is, a NaN.
+
 	pure elemental function isInvalid(value) result(invalid)
 	
 		! Routine arguments
@@ -87,5 +107,19 @@ contains
 		invalid = isnan(value)
 		
 	end function isInvalid
+
+	pure elemental function isInvalid8(value) result(invalid)
+	
+		! Routine arguments
+		real(8), intent(in)	:: value	! Value to check
+		logical				:: invalid	! Check result
+		
+		! Locals
+		! -none-
+		
+		! Check validity
+		invalid = isnan(value)
+		
+	end function isInvalid8
 	
 end module pbl_base
