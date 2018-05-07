11209c452f7701e3c2b785508d61f2bb52eed141
Mauri Favaron <mafavaron@mac.com>
2018-05-07 21:37:09 +0200

    Update t_pbl_time.f90
    Test added for timeEncode

--- test/pbl_time/t_pbl_time.f90
+++ test/pbl_time/t_pbl_time.f90
@@ -16,6 +16,14 @@ program test_pbl_time
 	character(len=23)	:: sDateTime
 	integer				:: iRetCode
 	real(8)				:: rEpoch
+	integer				:: iEpoch
+	integer				:: iNumHours
+	integer				:: i
+	
+	real(8), dimension(:), allocatable	:: rvTimeStamp
+	integer, dimension(:), allocatable	:: ivTimeStamp
+	integer, dimension(:), allocatable	:: ivTimeCode1
+	integer, dimension(:), allocatable	:: ivTimeCode2
 	
 	! Time stamp related tests
 	print *, "Test 1 - Maximum/minimum date/time values."
@@ -31,16 +39,46 @@ program test_pbl_time
 	print *, "Maximum date-time: ", trim(sDateTime), "  (Return code = ", iRetCode, "  - Expected = 0)"
 	dt = DateTime(10000, 1, 1, 0, 0, 0.d0)	! Modern Fortran default constructor!
 	rEpoch = dt % toEpoch()
-	print *, "Maximum Epoc value: ", rEpoch, "  (Return code = ", iRetCode, "  - Expected = 0)"
+	print *, "Maximum Epoch value: ", rEpoch, "  (Return code = ", iRetCode, "  - Expected = 0)"
 	iRetCode  = dt % fromEpoch(253402300800.0d0)
 	sDateTime = dt % toIso()
 	print *, "Invalid date-time: ", trim(sDateTime), "  (Return code = ", iRetCode, "  - Expected = 1)"
 	iRetCode  = dt % fromEpoch(0.0d0)
 	sDateTime = dt % toIso()
-	print *, "Invalid date-time: ", trim(sDateTime), "  (Return code = ", iRetCode, "  - Expected = 0)"
+	print *, "Zero-epoch date-time: ", trim(sDateTime), "  (Return code = ", iRetCode, "  - Expected = 0)"
 	iRetCode  = dt % fromEpoch(-1.0d0)
 	sDateTime = dt % toIso()
 	print *, "Invalid date-time: ", trim(sDateTime), "  (Return code = ", iRetCode, "  - Expected = 1)"
 	print *
+	
+	! Test 3: Check the two forms of "timeEncode"
+	iNumHours = 72
+	allocate(rvTimeStamp(iNumHours), ivTimeStamp(iNumHours))
+	dt = DateTime(2018, 3, 8, 0, 0, 0)
+	rEpoch = dt % toEpoch()
+	iEpoch = floor(rEpoch)
+	rvTimeStamp = [(rEpoch + (i-1)*3600.d0, i = 1, iNumHours)]
+	ivTimeStamp = [(iEpoch + (i-1)*3600, i = 1, iNumHours)]
+	! Daily span, hourly step
+	iRetCode = timeEncode(rvTimeStamp, 86400, 3600, ivTimeCode1)
+	iRetCode = timeEncode(ivTimeStamp, 86400, 3600, ivTimeCode2)
+	print *, "i,  Idx.1,  Idx.2"
+	do i = 1, iNumHours
+		print *, i, ivTimeCode1(i), ivTimeCode2(i)
+	end do
+	print *
+	
+	! Test 4: Some invalid values
+	rvTimeStamp(3) = NaN_8
+	ivTimeStamp(3) = 0
+	! Daily span, hourly step
+	iRetCode = timeEncode(rvTimeStamp, 86400, 3600, ivTimeCode1)
+	iRetCode = timeEncode(ivTimeStamp, 86400, 3600, ivTimeCode2)
+	print *, "i,  Idx.1,  Idx.2"
+	do i = 1, iNumHours
+		print *, i, ivTimeCode1(i), ivTimeCode2(i)
+	end do
+	print *
+	deallocate(rvTimeStamp, ivTimeStamp, ivTimeCode1, ivTimeCode2)
 	
 end program test_pbl_time
