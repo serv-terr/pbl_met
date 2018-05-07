24a6d330dfc5ec68e0c1f4750a1a4de9b030469d
Mauri Favaron <mafavaron@mac.com>
2018-05-07 10:14:05 +0200

    Update pbl_time.f90
    Added internal function converting integer(8) time stamps to codes (indices plus 0 for invalids), for use in time-based aggregation functions (e.g. typical days).

--- core/pbl_time.f90
+++ core/pbl_time.f90
@@ -30,29 +30,10 @@ module pbl_time
 	public	:: SinSolarElevation			! Compute the sine of solar elevation angle
 	public	:: SolarDeclination				! Compute the solar declination
 	public	:: SunRiseSunSet				! Old PBL_MET evaluation of sun rise and sun set times (revised)
-	! 3. Data types
-	public	:: DateTime						! Class, supporting the new REAL(8) time stamps used in Time Series
-	
-	! Data types
-	type DateTime
-		integer	:: iYear
-		integer	:: iMonth
-		integer	:: iDay
-		integer	:: iHour
-		integer	:: iMinute
-		real(8)	:: rSecond
-	contains
-		! Constructors
-		procedure	:: fromEpoch	=> dtFromEpoch
-		! Converters
-		procedure	:: toEpoch		=> dtToEpoch
-		! Printers
-		procedure	:: toIso		=> dtToIso
-	end type DateTime
 	
 	! Internal constants
-	real(8), parameter	:: TIME_MIN = 0.0d0
-	real(8), parameter	:: TIME_MAX = 253402300800.0d0	! Smallest Epoch value corresponding to an illegal date (10000-01-01 00:00:00.000)
+	real(8), parameter	:: TIME_MIN =                0.
+	real(8), parameter	:: TIME_MAX = 9007199254740991.
 	
 contains
 
@@ -499,153 +480,49 @@ contains
 
 	end function SolarDeclination
 	
+	! *********************
+	! * Internal routines *
+	! *********************
 	
-	! **************************************
-	! * Member functions of DateTime class *
-	! **************************************
-
-    subroutine UnpackDate8(iJulianDay, iYear, iMonth, iDay)
-
-        ! Routine arguments
-        integer(8), intent(in)     :: iJulianDay
-        integer, intent(out)       :: iYear
-        integer, intent(out)       :: iMonth
-        integer, intent(out)       :: iDay
-
-        ! Locals
-        integer(8) :: iDeviation
-        integer(8) :: iPreJulianDay
-        integer(8) :: iPostJulianDay
-        integer(8) :: iYearIndex
-        integer(8) :: iMonthIndex
-        integer(8) :: iDayIndex
-        integer(8), parameter  :: LIMIT_JULIAN_DAY = 2299161
-        integer(8), parameter  :: CORRECTION_DAYS  = 1524
-
-        ! Unwind Pope Gregorius' day correction
-        if(iJulianDay >= LIMIT_JULIAN_DAY) then
-            iDeviation = floor(((iJulianDay-1867216_8)-0.25d0)/36524.25d0, kind=8)
-            iPreJulianDay = iJulianDay + iDeviation - iDeviation/4_8 + 1_8
-        else
-            iPreJulianDay = iJulianDay
-        end if
-        iPostJulianDay = iPreJulianDay + CORRECTION_DAYS
-
-        ! Compute time indices
-        iYearIndex  = floor(6680_8+((iPostJulianDay-2439870_8)-122.1d0)/YEAR_DURATION, kind=8)
-        iDayIndex   = 365_8*iYearIndex + iYearIndex/4_8
-        iMonthIndex = floor((iPostJulianDay - iDayIndex)/MONTH_DURATION, kind=8)
-
-        ! Deduce preliminary date from time indices
-        iDay = iPostJulianDay - floor(MONTH_DURATION*iMonthIndex, kind=8	) - iDayIndex
-        if(iMonthIndex > 13_8) then
-            iMonth = iMonthIndex - 13_8
-        else
-            iMonth = iMonthIndex - 1_8
-        end if
-        iYear = iYearIndex - 4715_8
-        if(iMonth > 2) iYear = iYear - 1
-
-    end subroutine UnpackDate8
-
-
-    function dtFromEpoch(this, rEpoch) result(iRetCode)
-
-        ! Routine arguments
-        class(DateTime), intent(out)	:: this
-        real(8), intent(in)				:: rEpoch
-        integer							:: iRetCode
-
-        ! Locals
-        integer(8)	:: iJulianDay
-        real(8) 	:: rTimeSeconds
-        
-        ! Assume success (will falsify on failure)
-        iRetCode = 0
-
-        ! Check parameter
-        if(rEpoch < TIME_MIN .or. rEpoch >= TIME_MAX) then
-			iRetCode = 1
-			this % iYear   = 1970
-			this % iMonth  = 1
-			this % iDay    = 1
-			this % iHour   = 0
-			this % iMinute = 0
-			this % rSecond = 0.d0
-            return
-        end if
-
-        ! Isolate the date and time parts
-        iJulianDay = floor(rEpoch/(24.d0*3600.d0), kind=8) + BASE_DAY_8
-        rTimeSeconds = mod(rEpoch, 24.d0*3600.d0)
-
-        ! Process the date part
-        call UnpackDate8(iJulianDay, this % iYear, this % iMonth, this % iDay)
-
-        ! Extract time from the time part
-        this % rSecond = mod(rTimeSeconds,60.d0)
-        rTimeSeconds = rTimeSeconds/60.d0
-        this % iMinute = mod(floor(rTimeSeconds, kind=8),60_8)
-        this % iHour   = floor(rTimeSeconds, kind=8)/60_8
-
-    end function dtFromEpoch
-    
-
-    function dtToEpoch(this) result(rEpoch)
-
-        ! Routine arguments
-        class(DateTime), intent(in)		:: this
-        real(8)							:: rEpoch
-
-        ! Locals
-        integer    :: iJulianDay
-        integer(8) :: iJulianSecond
-        
-        ! Check input parameters for validity
-        if( &
-            this % iYear   <= 0   .or. &
-            this % iMonth  < 1    .or. this % iMonth  >  12 .OR. &
-            this % iDay    < 1    .or. this % iDay    >  31 .OR. &
-            this % iHour   < 0    .or. this % iHour   >  23 .OR. &
-            this % iMinute < 0    .or. this % iMinute >  59 .OR. &
-            this % rSecond < 0.d0 .or. this % rSecond >= 60.d0 &
-        ) then
-            rEpoch = NaN_8
-            return
-        end if
-
-        ! Compute based Julian day
-        iJulianDay = JulianDay(this % iYear, this % iMonth, this % iDay) - BASE_DAY
-
-        ! Convert based Julian day to second, and add seconds from time,
-        ! regardless of hour type.
-        iJulianSecond = iJulianDay * 24_8 * 3600_8
-        rEpoch = iJulianSecond + this % rSecond + 60_4*(this % iMinute + 60_4*this % iHour)
-
-    end function dtToEpoch
-
-    
-    function dtToIso(this) result(sDateTime)
-    
+	function timeEncode1(rvTimeStamp, iPeriodLength, iStepSize, ivTimeCode) result(iRetCode)
+	
 		! Routine arguments
-		class(DateTime), intent(in)	:: this
-		character(len=23)			:: sDateTime
+		real(8), intent(in), dimension(:)				:: rvTimeStamp
+		integer, intent(in)								:: iPeriodLength
+		integer, intent(in)								:: iStepSize
+		integer, intent(out), dimension(:), allocatable	:: ivTimeCode
+		integer											:: iRetCode
 		
 		! Locals
-		integer	:: iSecond
-		real(8)	:: rSubSecond
+		integer		:: n
+		integer		:: i
+		integer(8)	:: iTimeStamp
 		
-		! Write the data desired
-		iSecond = floor(this % rSecond)
-		rSubSecond = this % rSecond - iSecond
-		write(sDateTime, "(i4.4,2('-',i2.2),1x,i2.2,2(':',i2.2),f4.3)") &
-			this % iYear, &
-			this % iMonth, &
-			this % iDay, &
-			this % iHour, &
-			this % iMinute, &
-			iSecond, rSubSecond
-			
-    end function dtToIso
+		! Assume success (will falsify on failure)
+		iRetCode = 0
+		
+		! Reserve workspace
+		if(allocated(ivTimeCode)) deallocate(ivTimeCode)
+		n = size(rvTimeStamp)
+		if(n <= 0) then
+			iRetCode = 1
+			return
+		end if
+		allocate(ivTimeCode(n))
+		
+		! Iterate over time stamps, and assign codes
+		do i = 1, n
+			if(.valid.rvTimeStamp(i)) then
+				iTimeStamp = floor(rvTimeStamp(i), kind=8)
+				ivTimeCode(i) = mod(iTimeStamp, iPeriodLength) / iStepSize + 1
+			else
+				ivTimeCode(i) = 0	! Special "invalid" code
+			end if
+		end do
+		
+		! Leave
+		deallocate(ivTimeCode)
+		
+	end function timeEncode1
 
 end module pbl_time
