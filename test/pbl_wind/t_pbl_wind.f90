d193779f972762bb85f8f37a14b029500f98f8b6
Mauri Favaron <mafavaron@mac.com>
2018-05-11 18:52:25 +0200

    Create t_pbl_wind.f90
    Created test file for pbl_wind module, with first test about converting from 2d Cartesian to polar.

---
+++ test/pbl_wind/t_pbl_wind.f90
@@ -0,0 +1,54 @@
+! t_pbl_wind - Test procedure for pbl_wind sub-library
+!
+! This module is part of the pbl_met library.
+!
+! Copyright 2018 by Servizi Territorio srl
+!
+! This is open-source code, covered by the lGPL 3.0 license.
+!
+program t_pbl_wind
+
+	use pbl_met
+	
+	implicit none
+	
+	! Locals
+	
+	! Constants
+	real, parameter	:: pi = 3.1415927
+	
+	! Perform actual tests
+	call polar_cartesian()
+	
+contains
+
+	subroutine polar_cartesian()
+	
+		! Routine arguments
+		! --none--
+		
+		! Locals
+		real, dimension(2)	:: rvCartesian2
+		real, dimension(3)	:: rvCartesian3
+		real, dimension(2)	:: rvPolar2
+		real, dimension(3)	:: rvPolar3
+		integer				:: i
+		real				:: dir
+		real				:: e, n
+		
+		print *, "Test 1 - Check Cartesian to Polar conversions - Same convention, speed 1"
+		print *, "Expected.Vel, Expected.Dir, Result.Vel, Result.Dir"
+		do i = 0, 7
+			dir = 360./8. * i
+			e   = sin(dir*pi/180.)
+			n   = cos(dir*pi/180.)
+			rvCartesian2 = [e, n]
+			rvPolar2 = CartesianToPolar2(rvCartesian2, WCONV_SAME)
+			print "('  1.0, ',f4.0,', ',f6.3,', ',f8.3)", &
+				dir, rvPolar2
+		end do
+		print *
+		
+	end subroutine polar_cartesian
+
+end program t_pbl_wind
