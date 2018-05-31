c2a6c7ca55c120836020e0ed74f4d41f01c2badb
Mauri Favaron <mafavaron@mac.com>
2018-05-31 22:38:11 +0200

    Update pbl_base.f90
    IniFile % read member has been written.

--- core/pbl_base.f90
+++ core/pbl_base.f90
@@ -35,6 +35,7 @@ module pbl_base
 	public	:: toUpper
 	public	:: toLower
 	! 2. Data types
+	public	:: IniFile
 	
 	! Constants
     real, parameter		:: NaN				       = Z'7FC00000'			! Special case of non-signalling NaN (single precision)
@@ -69,9 +70,10 @@ module pbl_base
 	
 	type IniFile
 		logical, private										:: lIsUseable
+		integer, private										:: iNumKeys
 		character(len=256), dimension(:), allocatable, private	:: svLine
-		character(len=256), dimension(:), allocatable, private	:: svSections
-		character(len=256), dimension(:), allocatable, private	:: svKeyValue
+		character(len=256), dimension(:), allocatable, private	:: svKey
+		character(len=256), dimension(:), allocatable, private	:: svValue
 	contains
 		! Constructor
 		procedure, public	:: read       => iniRead
@@ -196,9 +198,106 @@ contains
 		integer							:: iRetCode
 		
 		! Locals
+		integer				:: iErrCode
+		character(len=256)	:: sBuffer
+		character(len=256)	:: sCurrentSection
+		character(len=256)	:: sCurSection
+		integer				:: iNumLines
+		integer				:: iLine
+		integer				:: iPos
+		integer				:: iNumKeys
+		integer				:: i
 		
 		! Assume success (will falsify on failure)
 		iRetCode = 0
+		
+		! Clean state before to proceed
+		this % lIsUseable = .false.
+		if(allocated(this % svLine)) deallocate(this % svLine)
+		if(allocated(this % svKey)) deallocate(this % svKey)
+		if(allocated(this % svValue)) deallocate(this % svValue)
+		
+		! Now, count lines excluding comments
+		open(iLUN, file=sIniFileName, status='old', action='read', iostat=iErrCode)
+		if(iErrCode /= 0) then
+			iRetCode = 1
+			return
+		end if
+		iNumLines = 0
+		do
+		
+			! Try gathering a line, and if acquired replace all characters
+			! from the first '#' on with blanks
+			read(iLun, "(a)", iostat=iErrCode) sBuffer
+			if(iErrCode /= 0) exit
+			iPos = index(sBuffer, "#")
+			if(iPos > 0) sBuffer(iPos:) = ' '
+			
+			! Replace TABs and other spaces with regular blanks
+			do i = 1, len(sBuffer)
+				if(ichar(sBuffer(i:i)) < 32) sBuffer(i:i) = ' ' 
+			end do
+			if(sBuffer == ' ') cycle
+			! Post-condition: something remains
+			
+			! Increment line count, remembering lines which may be subject to parsing
+			iNumLines = iNumLines + 1
+			
+		end do
+		if(iNumLines <= 0) then
+			close(iLun)
+			iRetCode = 2
+			return
+		end if
+		rewind(iLUN)
+		
+		! Reserve workspace, and populate it with non-comment lines
+		allocate(this % svLine(iNumLines), this % svKey(iNumLines), this % svValue(iNumLines))
+		iLine = 0
+		do
+		
+			! Try gathering a line, and if acquired replace all characters
+			! from the first '#' on with blanks
+			read(iLun, "(a)", iostat=iErrCode) sBuffer
+			if(iErrCode /= 0) exit
+			iPos = index(sBuffer, "#")
+			if(iPos > 0) sBuffer(iPos:) = ' '
+			
+			! Replace TABs and other spaces with regular blanks
+			do i = 1, len(sBuffer)
+				if(ichar(sBuffer(i:i)) < 32) sBuffer(i:i) = ' ' 
+			end do
+			if(sBuffer == ' ') cycle
+			! Post-condition: something remains
+			
+			! Add next line
+			iLine = iLine + 1
+			this % svLine(iLine) = sBuffer
+			
+		end do
+		close(iLUN)
+		! Post-condition: Some lines found
+		
+		! Parse line contents
+		sCurrentSection = ""
+		iNumKeys        = 0
+		do iLine = 1, iNumLines
+			
+			! Check string is a section, and if so assign it
+			if(isSection(this % svLine(iLine), sCurSection)) then
+				sCurrentSection = sCurSection
+			else
+				! Not a section: may contain an equal sign, that is, to be a name = value couple
+				iPos = index(this % svLine(iLine), "=")
+				if(iPos > 0) then
+					iNumKeys = iNumKeys + 1
+					write(this % svKey(iNumKeys), "(a,'@',a)") &
+						trim(sCurrentSection), adjustl(this % svLine(iLine)(1:(iPos-1)))
+					this % svValue(iNumKeys) = adjustl(this % svLine(iLine)((iPos+1):))
+				end if
+			end if
+			
+		end do
 		
 	end function iniRead
 	
@@ -303,5 +402,43 @@ contains
 		iRetCode = 0
 		
 	end function iniGetVector8
+	
+	! **********************
+	! * Internal functions *
+	! **********************
+	
+	function isSection(sString, sSection) result(lIsSection)
+	
+		! Routine arguments
+		character(len=*), intent(in)	:: sString
+		character(len=*), intent(out)	:: sSection
+		logical							:: lIsSection
+		
+		! Locals
+		integer		:: iPos
+		integer		:: iLast
+		
+		! Check first and last character are compatible with a section-type string
+		iPos = verify(sString, ' ')
+		iLast = len_trim(sString)
+		if(iPos >= 1 .and. iPos <= iLast) then
+			! Some blanks before the string real beginning: parse from there
+			lIsSection = sString(iPos:iPos) == '[' .and. sString(iLast:iLast) == ']'
+			if(lIsSection) then
+				sSection = sString((iPos+1):(iLast-1))
+			else
+				sSection = ' '
+			end if
+		else
+			! String begins with a non-blank
+			lIsSection = sString(1:1) == '[' .and. sString(iLast:iLast) == ']'
+			if(lIsSection) then
+				sSection = sString(1:(iLast-1))
+			else
+				sSection = ' '
+			end if
+		end if
+		
+	end function isSection
 	
 end module pbl_base
