0cc9c13c19cd6cb066843031b3c897059c685c72
Mauri Favaron <mafavaron@mac.com>
2018-05-31 14:28:51 +0200

    Create t_pbl_base.f90
    Added test program for pbl_base.f90, with placeholder for IniFile test type

---
+++ test/pbl_base/t_pbl_base.f90
@@ -0,0 +1,34 @@
+! The 'pbl_base' sub-library, part of 'pbl_met', contains functions used by other
+! sub-libraries and, occasionally, end-user code. The large part of it is tested
+! indirectly, by calling parts from other sub-libraries and their test programs.
+!
+! The present test program is aimed at testing a subset of the functions and
+! data types of pbl_base, which are easier to check in a systematic way.
+!
+! This program is part of the pbl_met project.
+!
+! pbl_met is an open-source library aimed at meteorological data processing
+! and meteorological processor crafting.
+!
+! Copyright 2018 by Servizi Territorio srl
+!
+program t_pbl_base
+
+	! Locals
+	
+	! Perform tests
+	call tstIniFile()
+
+contains
+
+	subroutine tstIniFile()
+	
+		! Routine arguments
+		! -none-
+		
+		! Locals
+		type(IniFile)	:: tIniFile
+		
+	end subroutine tstIniFile
+
+end program t_pbl_base
