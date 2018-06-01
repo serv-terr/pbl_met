8affe1a4092423b3fdbcd197f747602fd617dd2c
Mauri Favaron <mafavaron@mac.com>
2018-06-01 23:43:10 +0200

    Update pbl_base.f90
    Placeholder of IniFile % getReal4 filled with code: awaiting test now.

--- core/pbl_base.f90
+++ core/pbl_base.f90
@@ -1,556 +1,110 @@
-! pbl_base - Fortran module, containing useful symbols and constants.
+! The 'pbl_base' sub-library, part of 'pbl_met', contains functions used by other
+! sub-libraries and, occasionally, end-user code. The large part of it is tested
+! indirectly, by calling parts from other sub-libraries and their test programs.
 !
-! This module is part of the pbl_met library.
+! The present test program is aimed at testing a subset of the functions and
+! data types of pbl_base, which are easier to check in a systematic way.
+!
+! This program is part of the pbl_met project.
+!
+! pbl_met is an open-source library aimed at meteorological data processing
+! and meteorological processor crafting.
 !
 ! Copyright 2018 by Servizi Territorio srl
 !
-! This is open-source code, covered by the lGPL 3.0 license.
-!
-module pbl_base
+program t_pbl_base
 
+	use pbl_met
+	
 	implicit none
+
+	! Locals
 	
-	private
-	
-	! Public interface
-	! 0. Useful constants and symbols
-	public	:: NaN							! Non-signalling single precision NaN (generates other NaNs when combined with other values)
-	public	:: NaN_8						! Non-signalling double precision NaN (generates other NaNs when combined with other values)
-	public	:: LAI_GRASS
-	public	:: LAI_ALFALFA
-	public	:: ASCE_STANDARDATMOSPHERE
-	public	:: ASCE_STANDARDEQ
-	public	:: ASCE_MEANTEMPERATURE
-	public	:: ASCE_GRASS
-	public	:: ASCE_ALFALFA
-	public	:: ACV_GENERAL
-	public	:: ACV_2ND_ORDER
-	public	:: YEAR_DURATION
-	public	:: MONTH_DURATION
-	public	:: BASE_DAY
-	public	:: BASE_DAY_8
-	! 1. Operators and procedures
-	public	:: operator(.valid.)
-	public	:: operator(.invalid.)
-	public	:: toUpper
-	public	:: toLower
-	! 2. Data types
-	public	:: IniFile
-	
-	! Constants
-    real, parameter		:: NaN				       = Z'7FC00000'			! Special case of non-signalling NaN (single precision)
-    real(8), parameter	:: NaN_8			       = Z'7FF8000000000000'	! Special case of non-signalling NaN (double precision)
-	real, parameter		:: YEAR_DURATION	       = 365.25
-	real, parameter		:: MONTH_DURATION	       = 30.6001
-	integer, parameter	:: BASE_DAY			       = 2440588		! 01. 01. 1970
-	integer, parameter	:: BASE_DAY_8		       = 2440588_8		! 01. 01. 1970
-	integer, parameter	:: LAI_GRASS               = 0
-	integer, parameter	:: LAI_ALFALFA             = 1
-	integer, parameter	:: ASCE_STANDARDATMOSPHERE = 0
-	integer, parameter	:: ASCE_STANDARDEQ         = 1
-	integer, parameter	:: ASCE_MEANTEMPERATURE    = 2
-	integer, parameter	:: ASCE_GRASS              = 1
-	integer, parameter	:: ASCE_ALFALFA            = 2
-	integer, parameter	:: ACV_GENERAL             = 0
-	integer, parameter	:: ACV_2ND_ORDER           = 1
-	
-	! Operators
-	
-	interface operator(.valid.)
-		module procedure isValid
-		module procedure isValid8
-	end interface operator(.valid.)
-	
-	interface operator(.invalid.)
-		module procedure isInvalid
-		module procedure isInvalid8
-	end interface operator(.invalid.)
-	
-	! Data types
-	
-	type IniFile
-		logical, private										:: lIsUseable
-		integer, private										:: iNumKeys
-		character(len=256), dimension(:), allocatable, private	:: svLine
-		character(len=256), dimension(:), allocatable, private	:: svKey
-		character(len=256), dimension(:), allocatable, private	:: svValue
-	contains
-		! Constructor
-		procedure, public	:: read       => iniRead
-		procedure, public	:: dump       => iniDump
-		procedure, public	:: getString  => iniGetString
-		procedure, public	:: getReal4   => iniGetReal4
-		procedure, public	:: getReal8   => iniGetReal8
-		procedure, public	:: getInteger => iniGetInteger
-		procedure, public	:: getVector4 => iniGetVector4
-		procedure, public	:: getVector8 => iniGetVector8
-	end type IniFile
-	
+	! Perform tests
+	call tstIniFile()
+
 contains
 
-	! Check a value is valid, that is, not a NaN.
-	
-	pure elemental function isValid(value) result(valid)
+	subroutine tstIniFile()
 	
 		! Routine arguments
-		real, intent(in)	:: value	! Value to check
-		logical				:: valid	! Check result
-		
-		! Locals
 		! -none-
 		
-		! Check validity
-		valid = .not.isnan(value)
-		
-	end function isValid
-	
-	pure elemental function isValid8(value) result(valid)
-	
-		! Routine arguments
-		real(8), intent(in)	:: value	! Value to check
-		logical				:: valid	! Check result
-		
 		! Locals
-		! -none-
+		type(IniFile)		:: tIniFile
+		integer				:: iRetCode
+		character(len=256)	:: sValue
 		
-		! Check validity
-		valid = .not.isnan(value)
+		! Test 1: Load test configuration, then dump it
+		print *, 'Test 1 - Check INI read and decode on an existing file'
+		iRetCode = tIniFile % read(10, "test.ini")
+		if(iRetCode /= 0) then
+			print *, 'Test 1 failed: please identify and correct the malfunction'
+			stop
+		end if
+		iRetCode = tIniFile % dump()
+		print *
 		
-	end function isValid8
-	
+		! Test 2: get string value from INI
+		print *, 'Test 2: Get string values from INI'
+		print *
+		print *, 'Case 1: Get existent string, without default'
+		iRetCode = tIniFile % getString("Mysterious", "Boh", sValue)
+		if(iRetCode /= 0) then
+			print *, 'Error no.', iRetCode
+			stop
+		end if
+		print *,'Value: ', trim(sValue), '   (expected: SunChi)'
+		print *
+		print *, 'Case 2: Get existent string, with default'
+		iRetCode = tIniFile % getString("Mysterious", "Boh", sValue, 'Artificial_default')
+		if(iRetCode /= 0) then
+			print *, 'Error no.', iRetCode
+			stop
+		end if
+		print *,'Value: ', trim(sValue), '   (expected: SunChi)'
+		print *
+		print *, 'Case 3: Try loading non-existent string, no default'
+		iRetCode = tIniFile % getString("Nonsensical", "Wrong", sValue)
+		if(iRetCode /= 0) then
+			print *, 'Error no.', iRetCode
+			stop
+		end if
+		print *,'Value: ', trim(sValue), '   (expected: empty string)'
+		print *
+		print *, 'Case 4: Try loading non-existent string, with default'
+		iRetCode = tIniFile % getString("Nonsensical", "Wrong", sValue, "--missing--")
+		if(iRetCode /= 0) then
+			print *, 'Error no.', iRetCode
+			stop
+		end if
+		print *,'Value: ', trim(sValue), '   (expected: --missing--)'
+		print *
+		print *, 'Case 5: Try loading existent string, with empty section'
+		iRetCode = tIniFile % getString("", "here", sValue)
+		if(iRetCode /= 0) then
+			print *, 'Error no.', iRetCode
+			stop
+		end if
+		print *,'Value: ', trim(sValue), '   (expected: Here I am, amnyway)'
+		print *
+		print *, 'Case 6: Try loading existent string, with non-empty section and name but empty contents'
+		iRetCode = tIniFile % getString("Senseful", "Line_003", sValue, "Should not print this default")
+		if(iRetCode /= 0) then
+			print *, 'Error no.', iRetCode
+			stop
+		end if
+		print *,'Value: ', trim(sValue), '   (expected: empty string)'
+		print *
+		print *, 'Case 7: Checking case sensitiveness using a wrongly-cased key and default'
+		iRetCode = tIniFile % getString("Senseful", "LINE_005", sValue, "... nunc in scutella iaceo.")
+		if(iRetCode /= 0) then
+			print *, 'Error no.', iRetCode
+			stop
+		end if
+		print *,'Value: ', trim(sValue), '   (expected: ... nunc in scutella iaceo.)'
+		print *
+		
+	end subroutine tstIniFile
 
-	! Check a value is invalid, that is, a NaN.
-
-	pure elemental function isInvalid(value) result(invalid)
-	
-		! Routine arguments
-		real, intent(in)	:: value	! Value to check
-		logical				:: invalid	! Check result
-		
-		! Locals
-		! -none-
-		
-		! Check validity
-		invalid = isnan(value)
-		
-	end function isInvalid
-
-	pure elemental function isInvalid8(value) result(invalid)
-	
-		! Routine arguments
-		real(8), intent(in)	:: value	! Value to check
-		logical				:: invalid	! Check result
-		
-		! Locals
-		! -none-
-		
-		! Check validity
-		invalid = isnan(value)
-		
-	end function isInvalid8
-	
-	
-	subroutine toUpper(sString)
-	
-		! Routine arguments
-		character(len=*), intent(inout)	:: sString
-		
-		! Locals
-		integer		:: i
-		character	:: c
-		
-		! Change all alphabetic letters to uppercase
-		do i = 1, len_trim(sString)
-			c = sString(i:i)
-			if(c >= 'a' .and. c <= 'z') then
-				c = char(ichar(c) - ichar('a') + ichar('A'))
-			end if
-		end do
-		
-	end subroutine toUpper
-	
-
-	subroutine toLower(sString)
-	
-		! Routine arguments
-		character(len=*), intent(inout)	:: sString
-		
-		! Locals
-		integer		:: i
-		character	:: c
-		
-		! Change all alphabetic letters to lowercase
-		do i = 1, len_trim(sString)
-			c = sString(i:i)
-			if(c >= 'A' .and. c <= 'Z') then
-				c = char(ichar(c) - ichar('A') + ichar('a'))
-			end if
-		end do
-		
-	end subroutine toLower
-	
-	
-	function iniRead(this, iLUN, sIniFileName) result(iRetCode)
-	
-		! Routine arguments
-		class(IniFile), intent(inout)	:: this
-		integer, intent(in)				:: iLUN
-		character(len=*), intent(in)	:: sIniFileName
-		integer							:: iRetCode
-		
-		! Locals
-		integer				:: iErrCode
-		character(len=256)	:: sBuffer
-		character(len=256)	:: sCurrentSection
-		character(len=256)	:: sCurSection
-		integer				:: iNumLines
-		integer				:: iLine
-		integer				:: iPos
-		integer				:: iNumKeys
-		integer				:: i
-		
-		! Assume success (will falsify on failure)
-		iRetCode = 0
-		
-		! Clean state before to proceed
-		this % lIsUseable = .false.
-		if(allocated(this % svLine)) deallocate(this % svLine)
-		if(allocated(this % svKey)) deallocate(this % svKey)
-		if(allocated(this % svValue)) deallocate(this % svValue)
-		
-		! Now, count lines excluding comments
-		open(iLUN, file=sIniFileName, status='old', action='read', iostat=iErrCode)
-		if(iErrCode /= 0) then
-			iRetCode = 1
-			return
-		end if
-		iNumLines = 0
-		do
-		
-			! Try gathering a line, and if acquired replace all characters
-			! from the first '#' on with blanks
-			read(iLun, "(a)", iostat=iErrCode) sBuffer
-			if(iErrCode /= 0) exit
-			iPos = index(sBuffer, "#")
-			if(iPos > 0) sBuffer(iPos:) = ' '
-			
-			! Replace TABs and other spaces with regular blanks
-			do i = 1, len(sBuffer)
-				if(ichar(sBuffer(i:i)) < 32) sBuffer(i:i) = ' ' 
-			end do
-			if(sBuffer == ' ') cycle
-			! Post-condition: something remains
-			
-			! Increment line count, remembering lines which may be subject to parsing
-			iNumLines = iNumLines + 1
-			
-		end do
-		if(iNumLines <= 0) then
-			close(iLun)
-			iRetCode = 2
-			return
-		end if
-		rewind(iLUN)
-		
-		! Reserve workspace, and populate it with non-comment lines
-		allocate(this % svLine(iNumLines), this % svKey(iNumLines), this % svValue(iNumLines))
-		iLine = 0
-		do
-		
-			! Try gathering a line, and if acquired replace all characters
-			! from the first '#' on with blanks
-			read(iLun, "(a)", iostat=iErrCode) sBuffer
-			if(iErrCode /= 0) exit
-			iPos = index(sBuffer, "#")
-			if(iPos > 0) sBuffer(iPos:) = ' '
-			
-			! Replace TABs and other spaces with regular blanks
-			do i = 1, len(sBuffer)
-				if(ichar(sBuffer(i:i)) < 32) sBuffer(i:i) = ' ' 
-			end do
-			if(sBuffer == ' ') cycle
-			! Post-condition: something remains
-			
-			! Add next line
-			iLine = iLine + 1
-			this % svLine(iLine) = sBuffer
-			
-		end do
-		close(iLUN)
-		! Post-condition: Some lines found
-		
-		! Parse line contents
-		sCurrentSection = ""
-		iNumKeys        = 0
-		do iLine = 1, iNumLines
-			
-			! Check string is a section, and if so assign it
-			if(isSection(this % svLine(iLine), sCurSection)) then
-				sCurrentSection = sCurSection
-			else
-				! Not a section: may contain an equal sign, that is, to be a name = value couple
-				iPos = index(this % svLine(iLine), "=")
-				if(iPos > 0) then
-					iNumKeys = iNumKeys + 1
-					write(this % svKey(iNumKeys), "(a,'@',a)") &
-						trim(sCurrentSection), adjustl(this % svLine(iLine)(1:(iPos-1)))
-					this % svValue(iNumKeys) = adjustl(this % svLine(iLine)((iPos+1):))
-					call removeChar(this % svValue(iNumKeys), '"')
-				end if
-			end if
-			
-		end do
-		
-		! Confirm successful completion
-		this % lIsUseable = .true.
-		this % iNumKeys   = iNumKeys
-		
-	end function iniRead
-	
-	
-	function iniDump(this) result(iRetCode)
-	
-		! Routine arguments
-		class(IniFile), intent(in)	:: this
-		integer						:: iRetCode
-		
-		! Locals
-		integer	:: i
-		integer	:: iKeyLen
-		
-		! Assume success (will falsify on failure)
-		iRetCode = 0
-		
-		! Check whether the dump is to be make in full,
-		! that is, the INI file read has completed successfully
-		! and the data structures have been filled
-		if(this % lIsUseable) then
-		
-			! Check length to constrain keys to when printing
-			iKeyLen = 0
-			do i = 1, this % iNumKeys
-				iKeyLen = max(iKeyLen, len_trim(this % svKey(i)))
-			end do
-		
-			! Print all keys, and their associated values. To print
-			! keys in column the maximum key length is used, along with
-			! the fact that in Fortran all strings in an array share
-			! the same length and are blank-filled on right. The approach
-			! I've followed would have *not* worked in C and other
-			! variable-length string languages.
-			do i = 1, this % iNumKeys
-				print "(a,' -> ',a)", this % svKey(i)(1:iKeyLen), trim(this % svValue(i))
-			end do
-		
-		else
-		
-			print *, "INI data contents has not yet been assigned, nothing to print"
-			iRetCode = 1
-			
-		end if
-		
-	end function iniDump
-	
-	
-	function iniGetString(this, sSection, sKey, sValue, sDefault) result(iRetCode)
-	
-		! Routine arguments
-		class(IniFile), intent(inout)			:: this
-		character(len=*), intent(in)			:: sSection
-		character(len=*), intent(in)			:: sKey
-		character(len=*), intent(out)			:: sValue
-		character(len=*), intent(in), optional	:: sDefault
-		integer									:: iRetCode
-		
-		! Locals
-		integer				:: i
-		character(len=256)	:: sFullKey
-		
-		! Assume success (will falsify on failure)
-		iRetCode = 0
-		
-		! Check something is to be made
-		if(this % iNumKeys > 0) then
-		
-			! Yes: there are data lines to scan
-			write(sFullKey, "(a, '@', a)") trim(sSection), trim(sKey)
-			do i = 1, this % iNumKeys
-				if(this % svKey(i) == sFullKey) then
-					sValue = this % svValue(i)
-					return
-				end if
-			end do
-			
-			! Nothing found if execution reaches here: in case,
-			! yield the default (if present) or a blank (otherwise).
-			if(present(sDefault)) then
-				sValue = sDefault
-			else
-				sValue = ' '
-			end if
-			
-		else
-			
-			! No INI data available: flag an error condition.
-			if(present(sDefault)) then
-				sValue = sDefault
-			else
-				sValue = ' '
-			end if
-			iRetCode = 1
-			
-		end if
-		
-	end function iniGetString
-	
-	
-	function iniGetReal4(this, sKey, rValue, rDefault) result(iRetCode)
-	
-		! Routine arguments
-		class(IniFile), intent(inout)			:: this
-		character(len=*), intent(in)			:: sKey
-		real, intent(out)						:: rValue
-		real, intent(in), optional				:: rDefault
-		integer									:: iRetCode
-		
-		! Locals
-		
-		! Assume success (will falsify on failure)
-		iRetCode = 0
-		
-	end function iniGetReal4
-	
-	
-	function iniGetReal8(this, sKey, rValue, rDefault) result(iRetCode)
-	
-		! Routine arguments
-		class(IniFile), intent(inout)			:: this
-		character(len=*), intent(in)			:: sKey
-		real(8), intent(out)					:: rValue
-		real(8), intent(in), optional			:: rDefault
-		integer									:: iRetCode
-		
-		! Locals
-		
-		! Assume success (will falsify on failure)
-		iRetCode = 0
-		
-	end function iniGetReal8
-	
-	
-	function iniGetInteger(this, sKey, iValue, iDefault) result(iRetCode)
-	
-		! Routine arguments
-		class(IniFile), intent(inout)			:: this
-		character(len=*), intent(in)			:: sKey
-		integer, intent(out)					:: iValue
-		integer, intent(in), optional			:: iDefault
-		integer									:: iRetCode
-		
-		! Locals
-		
-		! Assume success (will falsify on failure)
-		iRetCode = 0
-		
-	end function iniGetInteger
-	
-	
-	function iniGetVector4(this, sKey, rvValue, rvDefault) result(iRetCode)
-	
-		! Routine arguments
-		class(IniFile), intent(inout)				:: this
-		character(len=*), intent(in)				:: sKey
-		real, dimension(:), allocatable, intent(in)	:: rvValue
-		real, dimension(:), intent(in), optional	:: rvDefault
-		integer										:: iRetCode
-		
-		! Locals
-		
-		! Assume success (will falsify on failure)
-		iRetCode = 0
-		
-	end function iniGetVector4
-	
-	
-	function iniGetVector8(this, sKey, rvValue, rvDefault) result(iRetCode)
-	
-		! Routine arguments
-		class(IniFile), intent(inout)					:: this
-		character(len=*), intent(in)					:: sKey
-		real(8), dimension(:), allocatable, intent(in)	:: rvValue
-		real(8), dimension(:), intent(in), optional		:: rvDefault
-		integer											:: iRetCode
-		
-		! Locals
-		
-		! Assume success (will falsify on failure)
-		iRetCode = 0
-		
-	end function iniGetVector8
-	
-	! **********************
-	! * Internal functions *
-	! **********************
-	
-	function isSection(sString, sSection) result(lIsSection)
-	
-		! Routine arguments
-		character(len=*), intent(in)	:: sString
-		character(len=*), intent(out)	:: sSection
-		logical							:: lIsSection
-		
-		! Locals
-		integer		:: iPos
-		integer		:: iLast
-		
-		! Check first and last character are compatible with a section-type string
-		iPos = verify(sString, ' ')
-		iLast = len_trim(sString)
-		if(iPos >= 1 .and. iPos <= iLast) then
-			! Some blanks before the string real beginning: parse from there
-			lIsSection = sString(iPos:iPos) == '[' .and. sString(iLast:iLast) == ']'
-			if(lIsSection) then
-				sSection = sString((iPos+1):(iLast-1))
-			else
-				sSection = ' '
-			end if
-		else
-			! String begins with a non-blank
-			lIsSection = sString(1:1) == '[' .and. sString(iLast:iLast) == ']'
-			if(lIsSection) then
-				sSection = sString(1:(iLast-1))
-			else
-				sSection = ' '
-			end if
-		end if
-		
-	end function isSection
-	
-	
-	subroutine removeChar(sString, cChar)
-	
-		! Routine arguments
-		character(len=*), intent(inout)	:: sString
-		character, intent(in)			:: cChar
-		
-		! Locals
-		integer	:: i, j, n
-		
-		! Copy all desired characters, and them only, to the string, in place
-		n = len_trim(sString)
-		j = 0
-		do i = 1, n
-			if(sString(i:i) /= cChar) then
-				j = j + 1
-				if(j /= i) then
-					sString(j:j) = sString(i:i)
-				end if
-			end if
-		end do
-		if(j < n) then
-			sString((j+1):n) = ' '
-		end if
-		
-	end subroutine removeChar
-	
-end module pbl_base
+end program t_pbl_base
