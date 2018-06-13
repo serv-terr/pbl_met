d91bc5e0159ca55e2227d92ad7d0c9f53f193b81
Mauri Favaron <mafavaron@mac.com>
2018-06-13 09:59:21 +0200

    Update t_pbl_time.f90
    Test added for linear time index, daily, new time stamp

--- test/pbl_time/t_pbl_time.f90
+++ test/pbl_time/t_pbl_time.f90
@@ -137,6 +137,8 @@ contains
 		integer								:: iYear, iMonth, iDay, iHour, iMinute, iSecond
 		character(len=19)					:: sDateTime1
 		character(len=19)					:: sDateTime2
+		character(len=23)					:: sDateTime3
+		character(len=23)					:: sDateTime4
 		
 		! Test 1: Linear index under normal conditions, legacy time stamp
 		call PackTime(iCurTime, 2000, 1, 1, 0, 0, 0)
@@ -155,8 +157,27 @@ contains
 			write(sDateTime1, "(i4.4,2('-',i2.2),1x,i2.2,2(':',i2.2))") iYear, iMonth, iDay, iHour, iMinute, iSecond
 			call UnpackTime(ivBaseTimeStamp(i), iYear, iMonth, iDay, iHour, iMinute, iSecond)
 			write(sDateTime2, "(i4.4,2('-',i2.2),1x,i2.2,2(':',i2.2))") iYear, iMonth, iDay, iHour, iMinute, iSecond
-			print "(a,',',i8,',',a)", sDateTime1, ivTimeCode(i), sDateTime2
+			print "(a,',',i8,', ',a)", sDateTime1, ivTimeCode(i), sDateTime2
 		end do
+		print *
+		
+		! Test 2: Linear index under normal conditions, new time stamp
+		print *, "Test 2: Linear index, daily, legacy time stamp"
+		iRetCode = timeLinearIndex(rvTimeStamp, DELTA_1_DAY, ivTimeCode, rvBaseTimeStamp)
+		print *, "Return code: ", iRetCode, " (expected: 0)"
+		print *
+		print *, "Time.Stamp, Time.Index, Base.Time.Stamp"
+		do i = 1, 48
+			iRetCode = dt % fromEpoch(rvTimeStamp(i))
+			sDateTime3 = dt % toIso()
+			iRetCode = dt % fromEpoch(rvBaseTimeStamp(i))
+			sDateTime4 = dt % toIso()
+			print "(a,',',i8,', ',a)", sDateTime3, ivTimeCode(i), sDateTime4
+		end do
+		print *
+		
+		! Leave
+		deallocate(ivTimeStamp, rvTimeStamp, ivTimeCode, rvBaseTimeStamp, ivBaseTimeStamp)
 		
 	end subroutine tst_LinearIndex
 	
