7cb0f76749c45a14ff27d0a685442e2b6224a83c
Mauri Favaron <mafavaron@mac.com>
2018-06-14 06:43:05 +0200

    Update t_pbl_wind.f90
    Tests added for unrelated roses in CompareWindRoses()

--- test/pbl_wind/t_pbl_wind.f90
+++ test/pbl_wind/t_pbl_wind.f90
@@ -1036,7 +1036,7 @@ contains
 		integer								:: iRetCode
 		real								:: rProb
 		
-		! Test 1: Read and count an existing SonicLib file name
+		! Test 1: Identical roses
 		print *, "Test 1: Identical roses"
 		allocate(vel1(1024),dir1(1024),vel2(1024),dir2(1024))
 		call random_number(vel1)
@@ -1045,6 +1045,40 @@ contains
 		dir1 = 360.*dir1
 		vel2 = vel1
 		dir2 = dir1
+		iRetCode = CompareWindRoses(&
+			vel1, dir1, vel2, dir2, &
+			[1., 2., 4.5, 7.], &
+			16, WDCLASS_ZERO_CENTERED, &
+			rmWindRose1, rmWindRose2, rProb &
+		)
+		print *, "Ret.code = ", iRetCode, "  (expected: 0)"
+		print *, "Prob = ", rProb
+		print *
+		
+		! Test 2: Two roses all zero but on different classes
+		print *, "Test 2: Quite unrelated roses"
+		vel1 = 1.5
+		dir1 = 0.
+		vel2 = 10.
+		dir2 = 180.
+		iRetCode = CompareWindRoses(&
+			vel1, dir1, vel2, dir2, &
+			[1., 2., 4.5, 7.], &
+			16, WDCLASS_ZERO_CENTERED, &
+			rmWindRose1, rmWindRose2, rProb &
+		)
+		print *, "Ret.code = ", iRetCode, "  (expected: 0)"
+		print *, "Prob = ", rProb
+		print *
+		
+		! Test 3: Two unrelated roses
+		print *, "Test 3: Unrelated roses"
+		call random_number(vel1)
+		call random_number(dir1)
+		vel1 = 10.*vel1
+		dir1 = 360.*dir1
+		vel2 = 10.
+		dir2 = 180.
 		iRetCode = CompareWindRoses(&
 			vel1, dir1, vel2, dir2, &
 			[1., 2., 4.5, 7.], &
