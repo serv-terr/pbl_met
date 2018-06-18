c55cc6492bb5d12a4beebaa97a31dfeac268454e
Mauri Favaron <mafavaron@mac.com>
2018-06-18 18:09:48 +0200

    Update t_pbl_wind.f90
    Test added for VelDirMean: All invalid scalars.

--- test/pbl_wind/t_pbl_wind.f90
+++ test/pbl_wind/t_pbl_wind.f90
@@ -1391,6 +1391,22 @@ contains
 		end do
 		print *
 		
+		! Test 5: Classify a direction-dependent scalar, with all invalid scalar
+		call random_number(dir)
+		dir = dir * 360.
+		call random_number(vel)
+		vel = vel * 5.
+		scalar = NaN
+		print *, 'Test 5: Classify direction-dependent scalar, with all invalid scalar'
+		iRetCode = VelDirMean(vel, dir, scalar, [0.5, 1.5, 2.5, 3.5, 4.5], 16, WDCLASS_ZERO_BASED, rmMean)
+		print *, 'Return code: ', iRetCode, '   (expected: 0)'
+		print *
+		print *, 'Dir.cls, Min(mean(scalar)), Max(mean(scalar))'
+		do i = 1, 16
+			print *, i, minval(rmMean(:,i)), maxval(rmMean(:,i))
+		end do
+		print *
+		
 		! Leave
 		deallocate(vel, dir, scalar)
 		
