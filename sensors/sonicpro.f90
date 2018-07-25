d8aeb6f943d956743055ee8ccc94a8d777c06a0d
Mauri Favaron <mafavaron@mac.com>
2018-07-25 06:22:56 +0200

    Create sonicpro.f90
    Placeholder added for sonic pro program

---
+++ sensors/sonicpro.f90
@@ -0,0 +1,12 @@
+! Program sonicpro - Minimalistic eddy covariance processing, illustrating use of Usa1 module and
+! pbl_met, for use on data produced by Servizi Territorio srl's "WindRecorder" loggers.
+!
+! This module is part of the pbl_met library.
+!
+! Copyright 2018 by Servizi Territorio srl
+!
+! This is open-source code, covered by lGPL 3.0 license
+!
+program sonicpro
+
+end program sonicpro
