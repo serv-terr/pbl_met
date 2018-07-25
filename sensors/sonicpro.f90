a1e5b3816ec81d6205ec7ee13956352bf4eaae84
Mauri Favaron <mafavaron@mac.com>
2018-07-25 06:40:06 +0200

    Update sonicpro.f90
    Code added to get input parameters

--- sensors/sonicpro.f90
+++ sensors/sonicpro.f90
@@ -9,4 +9,39 @@
 !
 program sonicpro
 
+	use pbl_met
+	use usa1
+	
+	implicit none
+	
+	! Locals
+	character(len=256)	:: sDataPath
+	character(len=256)	:: sOutputFile
+	character(len=19)	:: sFirstDateTime
+	character(len=19)	:: sLastDateTime
+	integer				:: iRetCode
+	integer				:: iNumHours
+	type(DateTime)		:: tFrom
+	type(DateTime)		:: tTo
+	real(8)				:: rFrom
+	real(8)				:: rTo
+	
+	! Get parameters
+	if(command_argument_count() /= 4) then
+		print *, "sonicpro - Minimalistic eddy covariance calculator"
+		print *
+		print *, "Usage:"
+		print *
+		print *, "  ./sonicpro <DataPath> <DateTimeFrom> <DateTimeTo> <OutputFile>"
+		print *
+		print *, "Program 'sonicpro' is part of the pbl_met project, and is"
+		print *, "released under an lGPL-3.0 open source license."
+		print *
+		stop
+	end if
+	call get_command_argument(1, sDataPath)
+	call get_command_argument(2, sFirstDateTime)
+	call get_command_argument(3, sLastDateTime)
+	call get_command_argument(4, sOutputFile)
+
 end program sonicpro
