76c35f84c0f790d287106eb6cc6c3322ed2d6163
Mauri Favaron <mafavaron@mac.com>
2018-08-13 20:35:55 +0200

    Update t_pbl_depth.f90
    In test 2, added value print at last convective hour (17, with index 18).

--- test/pbl_depth/t_pbl_depth.f90
+++ test/pbl_depth/t_pbl_depth.f90
@@ -40,14 +40,14 @@ program t_pbl_depth
 	! Test 2: Effect of number of substeps
 	iRetCode = Synthetize(24)
 	open(10, file="Zi_Test2.csv", status="unknown", action="write")
-	write(10, "('N.steps, Zi(14)')")
+	write(10, "('N.steps, Zi(14), Zi(18)')")
 	do j = 1, 101, 10
 		iRetCode = EstimateZi(rvTimeStamp, 0, 0., 0., 3600, rvTemp, rvUstar, rvH0, rvN, j, rvZi)
 		do i = 1, size(rvTimeStamp)
 			iRetCode = tStamp % fromEpoch(rvTimeStamp(i))
 			sISOdate = tStamp % toISO()
 		end do
-		write(10, "(i3,',',f8.4))") j, rvZi(15)
+		write(10, "(i3,',',f8.4,',',f8.4)") j, rvZi(15), rvZi(18)
 	end do
 	close(10)
 	
