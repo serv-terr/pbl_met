905a4a20b6a3511fb8e5997118e195a2a65b0c12
Mauri Favaron <mafavaron@mac.com>
2018-10-01 13:09:43 +0200

    Update amf.r
    Function updated with more comparison charts.

--- test/pbl_wind/AmeriFlux_Gold_Files/amf.r
+++ test/pbl_wind/AmeriFlux_Gold_Files/amf.r
@@ -12,12 +12,90 @@ get.pbm <- function() {
   return(d)
 }
 
+uu <- function() {
+  d <- get.amf()
+  e <- get.pbm()
+  test <- d$uu
+  new  <- e$uu
+  plot(test,new,xlab="SonicLib",ylab="pbl_met",cex=0.2,main="UU (m2/s2)")
+  abline(0,1)
+  out <- data.frame(soniclib=test, pbl_met=new)
+  return(out)
+}
+
+vv <- function() {
+  d <- get.amf()
+  e <- get.pbm()
+  test <- d$vv
+  new  <- e$vv
+  plot(test,new,xlab="SonicLib",ylab="pbl_met",cex=0.2,main="VV (m2/s2)")
+  abline(0,1)
+  out <- data.frame(soniclib=test, pbl_met=new)
+  return(out)
+}
+
+ww <- function() {
+  d <- get.amf()
+  e <- get.pbm()
+  test <- d$ww
+  new  <- e$ww
+  plot(test,new,xlab="SonicLib",ylab="pbl_met",cex=0.2,main="WW (m2/s2)")
+  abline(0,1)
+  out <- data.frame(soniclib=test, pbl_met=new)
+  return(out)
+}
+
+uv <- function() {
+  d <- get.amf()
+  e <- get.pbm()
+  test <- d$uu
+  new  <- e$vv
+  plot(test,new,xlab="SonicLib",ylab="pbl_met",cex=0.2,main="UV (m2/s2)")
+  abline(0,1)
+  out <- data.frame(soniclib=test, pbl_met=new)
+  return(out)
+}
+
+uw <- function() {
+  d <- get.amf()
+  e <- get.pbm()
+  test <- d$uu
+  new  <- e$ww
+  plot(test,new,xlab="SonicLib",ylab="pbl_met",cex=0.2,main="UW (m2/s2)")
+  abline(0,1)
+  out <- data.frame(soniclib=test, pbl_met=new)
+  return(out)
+}
+
+vw <- function() {
+  d <- get.amf()
+  e <- get.pbm()
+  test <- d$vv
+  new  <- e$ww
+  plot(test,new,xlab="SonicLib",ylab="pbl_met",cex=0.2,main="VW (m2/s2)")
+  abline(0,1)
+  out <- data.frame(soniclib=test, pbl_met=new)
+  return(out)
+}
+
 phi <- function() {
   d <- get.amf()
   e <- get.pbm()
   test <- d$phi
   new  <- e$phi
-  plot(test,new,xlab="SonicLib",ylab="pbl_met",main="phi (rad)")
+  plot(test,new,xlab="SonicLib",ylab="pbl_met,cex=0.2",main="phi (rad)")
+  abline(0,1)
+  out <- data.frame(soniclib=test, pbl_met=new)
+  return(out)
+}
+
+theta <- function() {
+  d <- get.amf()
+  e <- get.pbm()
+  test <- d$theta
+  new  <- e$theta
+  plot(test,new,xlab="SonicLib",ylab="pbl_met",cex=0.2,main="theta (rad)")
+  abline(0,1)
   out <- data.frame(soniclib=test, pbl_met=new)
   return(out)
 }
@@ -27,7 +105,8 @@ u.star <- function() {
   e <- get.pbm()
   test <- d$u.star
   new  <- e$u.star
-  plot(test,new,xlab="SonicLib",ylab="pbl_met",main="u* (m/s)")
+  plot(test,new,xlab="SonicLib",ylab="pbl_met",cex=0.2,main="u* (m/s)")
+  abline(0,1)
   out <- data.frame(soniclib=test, pbl_met=new)
   return(out)
 }
