aaa50b3b57f6da5ea75a57d6175a7912919ef630
Mauri Favaron <mafavaron@mac.com>
2019-01-22 17:03:16 +0100

    Update alamo_SnapsPlot.jl
    Run configuration parsing added

--- examples/alamo_SnapsPlot.jl
+++ examples/alamo_SnapsPlot.jl
@@ -21,6 +21,27 @@ sSnapPath = ARGS[1]
 sOutPath  = ARGS[2]
 
 # Get boundary information
+line = open(sSnapPath * "/guide.txt") do f
+	lines = readlines(f)
+	(lines[1])
+end
+blocks = split(line)
+xmin   = parse(Float64, blocks[1])
+xmax   = parse(Float64, blocks[2])
+ymin   = parse(Float64, blocks[3])
+ymax   = parse(Float64, blocks[4])
+zmin   = parse(Float64, blocks[5])
+zmax   = parse(Float64, blocks[6])
+dx     = parse(Float64, blocks[7])
+dy     = parse(Float64, blocks[8])
+tmax   = parse(Float64, blocks[9])
+dt     = parse(Float32, blocks[10])
+year   = parse(Int32, blocks[11])
+month  = parse(Int32, blocks[12])
+day    = parse(Int32, blocks[13])
+hour   = parse(Int32, blocks[14])
+minute = parse(Int32, blocks[15])
+second = parse(Float64, blocks[16])
 
 # Iterate over input path contents, and process them
 files = readdir(sSnapPath)
