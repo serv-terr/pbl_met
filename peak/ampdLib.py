043be85618a2a4e15385bedf25ef27073341dacb
Mauri Favaron <mafavaron@mac.com>
2019-12-29 09:59:54 +0100

    Create ampdLib.py
    My revised version, containing some dia prints and returns

---
+++ peak/ampdLib.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+__author__      = "Patti M. Favaron"
+__copyright__   = "Copyright 2019, Patti M. Favaron"
+__credits__     = ["Jeremy Karst"]
+__email__       = "mafavaron@mac.com"
+
+
+import numpy as np
+
+# AMPD function
+def ampd(sigInput, LSMlimit = 1):
+	"""Find the peaks in the signal with the AMPD algorithm.
+	
+		I just added some diagnostics, in order to understand innards.
+		
+		Python implementation by Luca Cerina (luca.cerina@polimi.it)
+		(on github, as "ampdLib" project)
+	
+		Original implementation by Felix Scholkmann et al. in
+		"An Efficient Algorithm for Automatic Peak Detection in 
+		Noisy Periodic and Quasi-Periodic Signals", Algorithms 2012,
+		 5, 588-603
+
+		Parameters
+		----------
+		sigInput: ndarray
+			The 1D signal given as input to the algorithm
+		lsmLimit: float
+			Wavelet transform limit as a ratio of full signal length.
+			Valid values: 0-1, the LSM array will no longer be calculated after this point
+			  which results in the inability to find peaks at a scale larger than this factor.
+			  For example a value of .5 will be unable to find peaks that are of period 
+			  1/2 * signal length, a default value of 1 will search all LSM sizes.
+
+		Returns
+		-------
+		pks: ndarray
+			The ordered array of peaks found in sigInput
+	"""
+		
+	# Create preprocessing linear fit	
+	sigTime = np.arange(0, len(sigInput))
+	
+	# Detrend
+	dtrSignal = (sigInput - np.polyval(np.polyfit(sigTime, sigInput, 1), sigTime)).astype(float)
+	
+	N = len(dtrSignal)
+	L = int(np.ceil(N*LSMlimit / 2.0)) - 1
+	
+	# Generate random matrix
+	LSM = np.ones([L,N], dtype='uint8')
+	
+	# Local minima extraction
+	for k in range(1, L):
+		LSM[k - 1, np.where((dtrSignal[k:N - k - 1] > dtrSignal[0: N - 2 * k - 1]) & (dtrSignal[k:N - k - 1] > dtrSignal[2 * k: N - 1]))[0]+k] = 0
+	
+	pks = np.where(np.sum(LSM[0:np.argmin(np.sum(LSM, 1)), :], 0)==0)[0]
+	return pks, LSM
+
+
