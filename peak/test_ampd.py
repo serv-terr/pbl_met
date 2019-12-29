e685d59ec5933b35772a413e112812f2c0196e0d
Mauri Favaron <mafavaron@mac.com>
2019-12-29 09:58:54 +0100

    Update test_ampd.py
    Revised, for understanding

--- peak/test_ampd.py
+++ peak/test_ampd.py
@@ -3,8 +3,6 @@
 
 import os
 import ampdLib
-import math
-import random
 
 if __name__ == "__main__":
 	
@@ -23,56 +21,4 @@ if __name__ == "__main__":
 	print(len(peaks))
 	print(peaks)
 	print(" ")
-	
-	# Test 2: 0.9
-	peaks = ampdLib.ampd(rvX, 0.9)
-	print("Test 2")
-	print(len(peaks))
-	print(peaks)
-	print(" ")
-	
-	# Test 3: 0.5
-	peaks = ampdLib.ampd(rvX, 0.5)
-	print("Test 3")
-	print(len(peaks))
-	print(peaks)
-	print(" ")
-	
-	# Test 4: 0.2
-	peaks = ampdLib.ampd(rvX, 0.2)
-	print("Test 4")
-	print(len(peaks))
-	print(peaks)
-	print(" ")
-	
-	# Test 5: 0.01
-	peaks = ampdLib.ampd(rvX, 0.01)
-	print("Test 5")
-	print(len(peaks))
-	print(peaks)
-	print(" ")
-	
-	# Generate new set
-	rvX = [math.sin((2*3.1415927*i)/50) for i in range(16384)]
-
-	# Run test 6: default
-	peaks = ampdLib.ampd(rvX)
-	print("Test 6")
-	print(len(peaks))
-	print(peaks)
-	print(" ")
-	
-	# Add some noise
-	random.seed(1)
-	for i in range(len(rvX)):
-		rvX[i] += random.random()/5.0
-
-	# Run test 7: default
-	peaks = ampdLib.ampd(rvX)
-	print("Test 7")
-	print(len(peaks))
-	print(peaks)
-	print(" ")
-	
-
 	
