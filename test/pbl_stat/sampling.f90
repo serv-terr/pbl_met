b45b5271ff0c7ed738b86c905d1455e0b588e4c4
Patrizia Favaron <mafavaron@mac.com>
2021-08-04 08:01:28 +0200

    Update sampling.f90
    Tests added for real*8 selector

--- test/pbl_stat/sampling.f90
+++ test/pbl_stat/sampling.f90
@@ -16,6 +16,12 @@ program test_sampling
     integer                             :: iRetCode
     integer                             :: n, m
     integer                             :: i
+    type(DateTime)                      :: tDateTime
+    type(DateTime)                      :: tDateFrom
+    type(DateTime)                      :: tDateTo
+    real(8)                             :: rBaseTime
+    real(8)                             :: rDateFrom
+    real(8)                             :: rDateTo
 
     ! Tests of sampling index
 
@@ -315,7 +321,7 @@ program test_sampling
     print *, iRetCode, ' - ', size(rvS4), ' - ', rvS4
     print *
 
-    ! Test 3 - Without repetitions, as of default
+    ! Test 3
     n = 10
     print *, "Test 3"
     if(allocated(rvP4)) deallocate(rvP4)
@@ -333,7 +339,7 @@ program test_sampling
     print *, iRetCode, ' - ', size(rvS4), ' - ', rvS4
     print *
 
-    ! Test 4 - Without repetitions, as of default
+    ! Test 4
     n = 10
     print *, "Test 4"
     if(allocated(rvP4)) deallocate(rvP4)
@@ -351,9 +357,9 @@ program test_sampling
     print *, iRetCode, ' - ', size(rvS4), ' - ', rvS4
     print *
 
-    ! Test 4 - Without repetitions, as of default
+    ! Test 5
     n = 10
-    print *, "Test 4"
+    print *, "Test 5"
     if(allocated(rvP4)) deallocate(rvP4)
     allocate(rvP4(n))
     if(allocated(rvA4)) deallocate(rvA4)
@@ -367,6 +373,266 @@ program test_sampling
     print *, "Expected return code = 0"
     iRetCode = Select(rvP4, rvA4, 3.0, NaN, rvS4)
     print *, iRetCode, ' - ', size(rvS4), ' - ', rvS4
+    print *
+
+    ! Test 6
+    n = 10
+    print *, "Test 6"
+    if(allocated(rvP4)) deallocate(rvP4)
+    allocate(rvP4(n))
+    if(allocated(rvA4)) deallocate(rvA4)
+    allocate(rvA4(n))
+    do i = 1, n
+        rvP4(i) = i
+    end do
+    rvA4 = 0.
+    rvA4(5) = 1.
+    rvA4(6) = 2.
+    print *, "Expected return code = 0"
+    iRetCode = Select(rvP4, rvA4, 1.5, 0.5, rvS4)
+    print *, iRetCode, ' - ', size(rvS4), ' - ', rvS4
+    print *
+
+    ! Test 7
+    n = 10
+    print *, "Test 7"
+    if(allocated(rvP4)) deallocate(rvP4)
+    allocate(rvP4(n))
+    if(allocated(rvA4)) deallocate(rvA4)
+    allocate(rvA4(n+1))
+    do i = 1, n
+        rvP4(i) = i
+    end do
+    rvA4 = 0.
+    rvA4(5) = 1.
+    rvA4(6) = 2.
+    print *, "Expected return code = Non-zero"
+    iRetCode = Select(rvP4, rvA4, 1.5, 0.5, rvS4)
+    print *, iRetCode
+    print *
+
+    ! Test 8
+    n = 10
+    print *, "Test 8"
+    if(allocated(rvP4)) deallocate(rvP4)
+    allocate(rvP4(n))
+    if(allocated(rvA4)) deallocate(rvA4)
+    allocate(rvA4(n))
+    do i = 1, n
+        rvP4(i) = i
+    end do
+    rvP4(5) = NaN
+    rvA4 = 0.
+    rvA4(5) = 1.
+    rvA4(6) = 2.
+    print *, "Expected return code = 0"
+    iRetCode = Select(rvP4, rvA4, 1.5, 0.5, rvS4)
+    print *, iRetCode, ' - ', size(rvS4), ' - ', rvS4
+    print *
+
+    ! Test 9
+    n = 10
+    print *, "Test 9"
+    if(allocated(rvP4)) deallocate(rvP4)
+    allocate(rvP4(n))
+    if(allocated(rvA4)) deallocate(rvA4)
+    allocate(rvA4(n))
+    do i = 1, n
+        rvP4(i) = i
+    end do
+    rvA4 = 0.
+    rvA4(5) = 1.
+    rvA4(6) = 2.
+    print *, "Expected return code = Non-zero"
+    iRetCode = Select(rvP4, rvA4, NaN, NaN, rvS4)
+    print *, iRetCode
+    print *
+
+    ! Test of selection - real*8
+
+    ! Test 1 - Without repetitions, as of default
+    n = 10
+    print *, "Test 1"
+    if(allocated(rvP8)) deallocate(rvP8)
+    allocate(rvP8(n))
+    if(allocated(rvA4)) deallocate(rvA4)
+    allocate(rvA8(n))
+    do i = 1, n
+        rvP8(i) = i
+    end do
+    rvA8 = 0.
+    rvA8(5) = 1.
+    rvA8(6) = 2.
+    print *, "Expected return code = 0"
+    iRetCode = Select(rvP8, rvA8, 0.5d0, NaN_8, rvS8)
+    print *, iRetCode, ' - ', size(rvS8), ' - ', rvS8
+    print *
+
+    ! Test 2 - Without repetitions, as of default
+    n = 10
+    print *, "Test 2"
+    if(allocated(rvP8)) deallocate(rvP8)
+    allocate(rvP8(n))
+    if(allocated(rvA8)) deallocate(rvA8)
+    allocate(rvA8(n))
+    do i = 1, n
+        rvP8(i) = i
+    end do
+    rvA8 = 0.
+    rvA8(5) = 1.
+    rvA8(6) = 2.
+    print *, "Expected return code = 0"
+    iRetCode = Select(rvP8, rvA8, NaN_8, 0.5d0, rvS8)
+    print *, iRetCode, ' - ', size(rvS8), ' - ', rvS8
+    print *
+
+    ! Test 3
+    n = 10
+    print *, "Test 3"
+    if(allocated(rvP8)) deallocate(rvP8)
+    allocate(rvP8(n))
+    if(allocated(rvA8)) deallocate(rvA8)
+    allocate(rvA8(n))
+    do i = 1, n
+        rvP8(i) = i
+    end do
+    rvA8 = 0.
+    rvA8(5) = 1.
+    rvA8(6) = 2.
+    print *, "Expected return code = 0"
+    iRetCode = Select(rvP8, rvA8, 0.5d0, 1.5d0, rvS8)
+    print *, iRetCode, ' - ', size(rvS8), ' - ', rvS8
+    print *
+
+    ! Test 4
+    n = 10
+    print *, "Test 4"
+    if(allocated(rvP8)) deallocate(rvP8)
+    allocate(rvP8(n))
+    if(allocated(rvA8)) deallocate(rvA8)
+    allocate(rvA8(n))
+    do i = 1, n
+        rvP8(i) = i
+    end do
+    rvA8 = 0.
+    rvA8(5) = 1.
+    rvA8(6) = 2.
+    print *, "Expected return code = 0"
+    iRetCode = Select(rvP8, rvA8, 1.0d0, 1.0d0, rvS8)
+    print *, iRetCode, ' - ', size(rvS8), ' - ', rvS8
+    print *
+
+    ! Test 5
+    n = 10
+    print *, "Test 5"
+    if(allocated(rvP8)) deallocate(rvP8)
+    allocate(rvP8(n))
+    if(allocated(rvA8)) deallocate(rvA8)
+    allocate(rvA8(n))
+    do i = 1, n
+        rvP8(i) = i
+    end do
+    rvA8 = 0.
+    rvA8(5) = 1.
+    rvA8(6) = 2.
+    print *, "Expected return code = 0"
+    iRetCode = Select(rvP8, rvA8, 3.0d0, NaN_8, rvS8)
+    print *, iRetCode, ' - ', size(rvS8), ' - ', rvS8
+    print *
+
+    ! Test 6
+    n = 10
+    print *, "Test 6"
+    if(allocated(rvP8)) deallocate(rvP8)
+    allocate(rvP8(n))
+    if(allocated(rvA8)) deallocate(rvA8)
+    allocate(rvA8(n))
+    do i = 1, n
+        rvP8(i) = i
+    end do
+    rvA8 = 0.
+    rvA8(5) = 1.
+    rvA8(6) = 2.
+    print *, "Expected return code = 0"
+    iRetCode = Select(rvP8, rvA8, 1.5d0, 0.5d0, rvS8)
+    print *, iRetCode, ' - ', size(rvS8), ' - ', rvS8
+    print *
+
+    ! Test 7
+    n = 10
+    print *, "Test 7"
+    if(allocated(rvP8)) deallocate(rvP8)
+    allocate(rvP8(n))
+    if(allocated(rvA8)) deallocate(rvA8)
+    allocate(rvA8(n+1))
+    do i = 1, n
+        rvP8(i) = i
+    end do
+    rvA8 = 0.
+    rvA8(5) = 1.
+    rvA8(6) = 2.
+    print *, "Expected return code = Non-zero"
+    iRetCode = Select(rvP8, rvA8, 1.5d0, 0.5d0, rvS8)
+    print *, iRetCode
+    print *
+
+    ! Test 8
+    n = 10
+    print *, "Test 8"
+    if(allocated(rvP8)) deallocate(rvP8)
+    allocate(rvP8(n))
+    if(allocated(rvA8)) deallocate(rvA8)
+    allocate(rvA8(n))
+    do i = 1, n
+        rvP8(i) = i
+    end do
+    rvP8(5) = NaN
+    rvA8 = 0.
+    rvA8(5) = 1.
+    rvA8(6) = 2.
+    print *, "Expected return code = 0"
+    iRetCode = Select(rvP8, rvA8, 1.5d0, 0.5d0, rvS8)
+    print *, iRetCode, ' - ', size(rvS8), ' - ', rvS8
+    print *
+
+    ! Test 9
+    n = 10
+    print *, "Test 9"
+    if(allocated(rvP8)) deallocate(rvP8)
+    allocate(rvP8(n))
+    if(allocated(rvA8)) deallocate(rvA8)
+    allocate(rvA8(n))
+    do i = 1, n
+        rvP8(i) = i
+    end do
+    rvA8 = 0.
+    rvA8(5) = 1.
+    rvA8(6) = 2.
+    print *, "Expected return code = Non-zero"
+    iRetCode = Select(rvP8, rvA8, NaN_8, NaN_8, rvS8)
+    print *, iRetCode
+    print *
+
+   ! Example 1
+    n = 48
+    print *, "Example 1"
+    if(allocated(rvP8)) deallocate(rvP8)
+    allocate(rvP8(n))
+    if(allocated(rvA8)) deallocate(rvA8)
+    allocate(rvA8(n))
+    tDateTime = DateTime(2000, 1, 1,  0, 0, 0.d0)
+    tDateFrom = DateTime(2000, 1, 1, 17, 0, 0.d0)
+    tDateTo   = DateTime(2000, 1, 2,  2, 0, 0.d0)
+    rBaseTime = tDateTime % toEpoch()
+    rDateFrom = tDateFrom % toEpoch()
+    rDateTo   = tDateTo % toEpoch()
+    do i = 1, n
+        rvP8(i) = i
+        rvA8(i) = rBaseTime + (i-1) * 3600.d0
+    end do
+    print *, "Expected return code = 0"
+    iRetCode = Select(rvP8, rvA8, rDateFrom, rDateTo, rvS8)
+    print *, iRetCode, ' - ', size(rvS8), ' - ', rvS8
     print *
 
 end program test_sampling
