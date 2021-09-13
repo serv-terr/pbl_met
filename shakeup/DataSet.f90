bd53e8a4b758b7d5bb6740bef3ef3c66551cb72d
Patrizia Favaron <mafavaron@mac.com>
2021-09-13 13:42:08 +0200

    Update DataSet.f90
    Corrections (various), in view of "shakeout"

--- shakeup/DataSet.f90
+++ shakeup/DataSet.f90
@@ -15,18 +15,29 @@ module DataSet
     ! User defined types
 
     type MetProDataSet
-        real                                :: lat
-        real                                :: lon
-        integer                             :: zone
-        real                                :: alt
-        real(8), dimension(:), allocatable  :: time_stamp_begin
-        real(8), dimension(:), allocatable  :: time_stamp_end
-        real(8), dimension(:), allocatable  :: Vel
-        real(8), dimension(:), allocatable  :: Dir
-        real(8), dimension(:), allocatable  :: Temp
-        real(8), dimension(:), allocatable  :: RelH
-        real(8), dimension(:), allocatable  :: ustar
-        real(8), dimension(:), allocatable  :: H0
+        real                                 :: lat
+        real                                 :: lon
+        integer                              :: zone
+        real                                 :: alt
+        real(8), dimension(:), allocatable   :: time_stamp_begin
+        real(8), dimension(:), allocatable   :: time_stamp_end
+        integer                              :: iDeltaTime
+        real(8), dimension(:), allocatable   :: Z
+        real(8), dimension(:,:), allocatable :: U
+        real(8), dimension(:,:), allocatable :: V
+        real(8), dimension(:,:), allocatable :: T
+        real(8), dimension(:), allocatable   :: Vel
+        real(8), dimension(:), allocatable   :: Dir
+        real(8), dimension(:), allocatable   :: Temp
+        real(8), dimension(:), allocatable   :: RelH
+        real(8), dimension(:), allocatable   :: Prec
+        real(8), dimension(:), allocatable   :: Pa
+        real(8), dimension(:), allocatable   :: Rg
+        real(8), dimension(:), allocatable   :: ustar
+        real(8), dimension(:), allocatable   :: H0
+        real(8), dimension(:), allocatable   :: L
+        real(8), dimension(:), allocatable   :: Cover
+        real(8), dimension(:), allocatable   :: Zi
     contains
         procedure   :: Read    => mp_DataRead
     end type MetProDataSet
@@ -70,28 +81,42 @@ contains
         type(StationList)                   :: tStations
         type(MultiSeries)                   :: tMultiSeries
         integer                             :: iErrCode
+        integer                             :: i
+        integer                             :: iDeltaTime
         real(8), dimension(:), allocatable  :: rvTimeStamp
+        real(8), dimension(:), allocatable  :: rvU
+        real(8), dimension(:), allocatable  :: rvV
+        real(8), dimension(:), allocatable  :: rvT
         real(8)                             :: rTimeDelta
+        real(8)                             :: rL
+        real(8)                             :: rWT
+        real(8)                             :: rTstar
 
         ! Steering constants
-        character(len=*), dimension(6), parameter   :: COL_NAME = [ &
+        character(len=*), dimension(8), parameter   :: COL_NAME = [ &
             'Temp     ', &
             'RelH     ', &
+            'Rg       ', &
+            'Prec     ', &
             'Vel_Sonic', &
             'Dir_Sonic', &
             'Ustar    ', &
             'H0       ' &
         ]
-        real(8), dimension(6), parameter            :: COL_THRESHOLD = [ &
+        real(8), dimension(8), parameter            :: COL_THRESHOLD = [ &
             9.d0, &
             9.d0, &
+            9.d0, &
+            0.d0, &
             9.d0, &
             9.d0, &
             0.d0, &
             0.d0 &
         ]
         
-        integer, dimension(6), parameter            :: COL_FILL = [ &
+        integer, dimension(8), parameter            :: COL_FILL = [ &
+            FILL_LINEAR, &
+            FILL_LINEAR, &
             FILL_LINEAR, &
             FILL_LINEAR, &
             FILL_LINEAR, &
@@ -166,6 +191,7 @@ contains
         allocate(this % time_stamp_end(size(rvTimeStamp)))
         this % time_stamp_begin = rvTimeStamp - rTimeDelta
         this % time_stamp_end   = rvTimeStamp
+        this % iDeltaTime       = nint(rvTimeStamp(2) - rvTimeStamp(1))
         
         ! Fill gaps
         iErrCode = tMultiSeries % fillGaps(COL_NAME, COL_FILL, 15)
@@ -175,9 +201,11 @@ contains
             return
         end if
 
-        ! Retrieve the four vectors
+        ! Retrieve the vectors
         iErrCode = tMultiSeries % getVector('Temp     ', this % temp)
         iErrCode = tMultiSeries % getVector('RelH     ', this % relh)
+        iErrCode = tMultiSeries % getVector('Rg       ', this % rg)
+        iErrCode = tMultiSeries % getVector('Prec     ', this % prec)
         iErrCode = tMultiSeries % getVector('Vel_Sonic', this % vel)
         iErrCode = tMultiSeries % getVector('Dir_Sonic', this % dir)
         iErrCode = tMultiSeries % getVector('Ustar    ', this % ustar)
@@ -187,6 +215,116 @@ contains
         where(this % ustar <= 0.05)
             this % ustar = 0.05
         endwhere
+        
+        ! Estimate pressure
+        if(allocated(this % Pa)) deallocate(this % Pa)
+        allocate(this % Pa(size(this % time_stamp_begin)))
+        this % Pa = AirPressure(this % alt)
+        
+        ! Estimate mixing height
+        if(allocated(this % Zi)) deallocate(this % Zi)
+        allocate(this % Zi(size(this % time_stamp_begin)))
+        iDeltaTime = int(rTimeDelta / 60.0)
+        iErrCode = EstimateZi( &
+            this % time_stamp_begin, &
+            this % zone, &
+            this % lat, &
+            this % lon, &
+            iDeltaTime, &
+            this % Temp, &
+            this % ustar, &
+            this % H0, &
+            rvZi = this % Zi &
+        )
+        if(iErrCode /= 0) then
+            print *, "DataSet % Read:: Error: Zi not estimated - Return code = ", iErrCode
+            iRetCode = 6
+            return
+        end if
+        
+        ! Assign cloud cover
+        if(allocated(this % Cover)) deallocate(this % Cover)
+        allocate(this % Cover(size(this % time_stamp_begin)))
+        where(this % Prec > 0.)
+            this % Cover = 1.0
+        elsewhere
+            this % Cover = 0.2
+        endwhere
+        
+        ! Assign heights and calculate vertical profiles
+        if(allocated(this % Zi)) deallocate(this % Zi)
+        allocate(this % Zi(size(this % time_stamp_begin)))
+        if(allocated(this % L)) deallocate(this % L)
+        allocate(this % L(size(this % time_stamp_begin)))
+        if(allocated(this % Z)) deallocate(this % Z)
+        if(allocated(rvU)) deallocate(rvU)
+        if(allocated(rvV)) deallocate(rvV)
+        if(allocated(rvT)) deallocate(rvT)
+        if(allocated(this % U)) deallocate(this % U)
+        if(allocated(this % V)) deallocate(this % V)
+        if(allocated(this % T)) deallocate(this % T)
+        allocate(this % Z(15))
+        allocate(rvU(15))
+        allocate(rvV(15))
+        allocate(rvT(15))
+        allocate(this % U(15, size(this % time_stamp_begin)))
+        allocate(this % V(15, size(this % time_stamp_begin)))
+        allocate(this % T(15, size(this % time_stamp_begin)))
+        this % Z = [(15.*i, i = 1, 15)]
+        do i = 1, size(this % time_stamp_begin)
+
+            ! Calculate Obukhov length and temperature scale
+            rWT = this % H0(i) / RhoCp(real(this % Temp(i), kind=4) + 273.15)
+            rL  = -this % ustar(i) ** 3 * (this % temp(i) + 273.15) / (0.4 * 9.807 * rWT)
+            if(.invalid.rL) rL = 100000.0
+            this % L(i) = min(max(rL, -100000.0), 100000.0)
+            rTstar = rWT / this % ustar(i)
+        
+            ! Estimate vertical wind profile
+            iErrCode = WindProfile( &
+                1, &
+                this % Z, &
+                10.d0, &
+                this % Vel(i), &
+                this % Dir(i), &
+                0.023d0, &
+                this % Zi(i), &
+                this % ustar(i), &
+                10.0d0/this % L(i), &
+                rvU, &
+                rvV &
+            )
+            if(iErrCode /= 0) then
+                print *, "DataSet % Read:: Error: wind profile not estimated - Return code = ", iErrCode
+                iRetCode = 7
+                return
+            end if
+            
+            ! Estimate vertical temperature profile
+            iErrCode = TempProfile( &
+                this % Z, &
+                0.023d0, &
+                10.d0, &
+                this % Temp(i) + 273.15, &
+                0.0098d0, &
+                this % Zi(i), &
+                rTstar, &
+                this % ustar(i), &
+                1.0d0/this % L(i), &
+                rvT &
+            )
+            if(iErrCode /= 0) then
+                print *, "DataSet % Read:: Error: temperature profile not estimated - Return code = ", iErrCode
+                iRetCode = 8
+                return
+            end if
+            
+            ! Assign vertical wind and temperature profiles
+            this % U(:,i) = rvU
+            this % V(:,i) = rvV
+            this % T(:,i) = rvT
+            
+        end do
 
     end function mp_DataRead
 
